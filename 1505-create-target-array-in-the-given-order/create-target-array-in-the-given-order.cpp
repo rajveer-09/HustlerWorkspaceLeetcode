c316bc13a3f2f2b0ccbc0e42a6314508efb03593
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2024-01-19 17:48:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (9.37%) - LeetSync

--- 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.cpp
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
-        int n= nums.size();
-        vector<int>ans(n,-1);
-
-        for (int i = 0; i < n; i++) {
-        if (ans[index[i]] == -1) {
-            ans[index[i]] = nums[i];
-        } else {
-            int current = ans[index[i]];
-            for (int j = n - 1; j > index[i]; j--) {
-                ans[j] = ans[j - 1];
-            }
-            ans[index[i] + 1] = current;
-            ans[index[i]] = nums[i];
+         int n = nums.size();
+        vector<int> target;
+        for (int i = 0; i < n; i++)
+        {
+            target.insert(target.begin() + index[i], nums[i]);
         }
-    }
-
-    return ans;
+        return target;
     }
 };
