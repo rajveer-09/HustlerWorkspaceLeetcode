18bcad6b14d09e33b036edf6971bd180fa515607
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-01-27 17:37:26 +0530

    Time: 10 ms (36.26%) | Memory: 9.9 MB (18.17%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,11 +1,53 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        string ths[]={"","M","MM","MMM"};
-        
-        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        string roman = "";
+    
+    // Thousands place
+    roman += string(num / 1000, 'M');
+    num %= 1000;
+    
+    // Hundreds place
+    if (num >= 900) {
+        roman += "CM";
+        num -= 900;
+    } else if (num >= 500) {
+        roman += "D";
+        num -= 500;
+    } else if (num >= 400) {
+        roman += "CD";
+        num -= 400;
+    }
+    roman += string(num / 100, 'C');
+    num %= 100;
+    
+    // Tens place
+    if (num >= 90) {
+        roman += "XC";
+        num -= 90;
+    } else if (num >= 50) {
+        roman += "L";
+        num -= 50;
+    } else if (num >= 40) {
+        roman += "XL";
+        num -= 40;
+    }
+    roman += string(num / 10, 'X');
+    num %= 10;
+    
+    // Ones place
+    if (num >= 9) {
+        roman += "IX";
+        num -= 9;
+    } else if (num >= 5) {
+        roman += "V";
+        num -= 5;
+    } else if (num >= 4) {
+        roman += "IV";
+        num -= 4;
+    }
+    roman += string(num, 'I');
+    
+    return roman;
     }
 };
