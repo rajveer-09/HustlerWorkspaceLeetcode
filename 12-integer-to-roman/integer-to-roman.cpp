79a8036b8abcae98ec904bd893aae27301ad7780
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-01-27 17:39:33 +0530

    Time: 12 ms (25.52%) | Memory: 13.4 MB (5.79%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,53 +1,20 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        string roman = "";
-    
-    // Thousands place
-    roman += string(num / 1000, 'M');
-    num %= 1000;
-    
-    // Hundreds place
-    if (num >= 900) {
-        roman += "CM";
-        num -= 900;
-    } else if (num >= 500) {
-        roman += "D";
-        num -= 500;
-    } else if (num >= 400) {
-        roman += "CD";
-        num -= 400;
+        vector<pair<int, string>> romanMap = {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+    };
+
+    // Iterate through the mapping to construct the Roman numeral string
+    string roman;
+    for (const auto& pair : romanMap) {
+        while (num >= pair.first) {
+            roman += pair.second;
+            num -= pair.first;
+        }
     }
-    roman += string(num / 100, 'C');
-    num %= 100;
-    
-    // Tens place
-    if (num >= 90) {
-        roman += "XC";
-        num -= 90;
-    } else if (num >= 50) {
-        roman += "L";
-        num -= 50;
-    } else if (num >= 40) {
-        roman += "XL";
-        num -= 40;
-    }
-    roman += string(num / 10, 'X');
-    num %= 10;
-    
-    // Ones place
-    if (num >= 9) {
-        roman += "IX";
-        num -= 9;
-    } else if (num >= 5) {
-        roman += "V";
-        num -= 5;
-    } else if (num >= 4) {
-        roman += "IV";
-        num -= 4;
-    }
-    roman += string(num, 'I');
-    
     return roman;
     }
 };
