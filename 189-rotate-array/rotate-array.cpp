4f79ace8930e4a1c64864c03c7536fdc991063c7
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-01-27 20:47:37 +0530

    Time: 22 ms (65.65%) | Memory: 27.3 MB (10.75%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
-    void reverse(vector<int>& nums, int start, int end) {
-    while (start < end) {
-        swap(nums[start], nums[end]);
-        start++;
-        end--;
-    }
-}
-
-void rotate(vector<int>& nums, int k) {
-    int n = nums.size();
-    k = k % n; // In case k is greater than the size of the array
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+    k %= n; // Calculate effective rotation count
 
     // Reverse the entire array
-    reverse(nums, 0, n - 1);
-    // Reverse the first k elements
-    reverse(nums, 0, k - 1);
-    // Reverse the remaining elements
-    reverse(nums, k, n - 1);
+    for (int i = 0; i < n / 2; ++i) {
+        swap(nums[i], nums[n - i - 1]);
+    }
 
+    // Reverse the first k elements
+    for (int i = 0; i < k / 2; ++i) {
+        swap(nums[i], nums[k - i - 1]);
+    }
+
+    // Reverse the remaining elements
+    for (int i = k; i < (n + k) / 2; ++i) {
+        swap(nums[i], nums[n - i - 1 + k]);
+    }
     }
 };
