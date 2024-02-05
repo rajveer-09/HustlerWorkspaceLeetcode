e4c52d65c1e07b1eeb76fb66507b32f49c66bc25
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-05 13:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (33.97%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     char repeatedCharacter(string s) {
-        vector<bool>oc(26,false);
+        bool oc[26]={false};
         for(int i=0;i<s.size();i++){
             if(oc[s[i]-'a']==true) return s[i];
             oc[s[i]-'a']=true;
