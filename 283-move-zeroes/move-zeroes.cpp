f841542e4d489ca6b45b7b6fcaf24d66c9a427a9
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-11 15:28:36 +0530

    Time: 20 ms (42.02%) | Memory: 21.5 MB (36.14%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,6 +4,7 @@ public:
         int n=nums.size();
         int i=0,j=0;
         while(j<n){
+            while(nums[i]!=0 &i<j) i++;
             if(nums[j]!=0){
                 swap(nums[i],nums[j]);
                 i++;
