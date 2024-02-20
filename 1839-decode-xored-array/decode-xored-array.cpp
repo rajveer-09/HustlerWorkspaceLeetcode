70fc6c0d6d836859e32a033c04c39f53800f1a19
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-20 14:55:02 +0530

    Time: 14 ms (95.32%) | Memory: 28.4 MB (24.10%) - LeetSync

--- 1839-decode-xored-array/decode-xored-array.cpp
+++ 1839-decode-xored-array/decode-xored-array.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
         int n=encoded.size();
-        vector<int>ans(n+1,0);
-        ans[0]=first;
+        vector<int>ans;
+        ans.push_back(first);
         for(int i=1;i<=n;i++){
-            ans[i]=(encoded[i-1]^ans[i-1]);
+            ans.push_back((encoded[i-1]^ans[i-1]));
         }
         return ans;
     }
