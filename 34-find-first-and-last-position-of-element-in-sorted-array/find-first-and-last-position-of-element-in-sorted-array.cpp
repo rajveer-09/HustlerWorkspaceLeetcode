fb348fe8dfc4283952fd9294cf6ed368eaf28388
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-21 18:38:25 +0530

    Time: 10 ms (21.54%) | Memory: 16 MB (28.12%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start=0, end=nums.size()-1;
-        int index=-1;
-        vector<int>ans;
-        int mid=0;
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                index=mid;
-                end=mid-1;
+        int start = 0, end = nums.size() - 1;
+        int startIdx = -1, endIdx = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] >= target) {
+                if (nums[mid] == target) startIdx = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
-            else if(nums[mid]>target) end=mid-1;
-            else start=mid+1;
         }
-        if(index==-1) return {-1,-1};
-        ans.push_back(index);
-        start = mid+1;
-        end = nums.size() - 1;
-        while(start<=end){
-            mid=start+(end-start)/2;
-            if(nums[mid]==target){
-                index=mid;
-                start=mid+1;
+        start = 0, end = nums.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= target) {
+                if (nums[mid] == target) endIdx = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-            else if(nums[mid]>target) end=mid-1;
-            else start=mid+1;
         }
-        ans.push_back(index);
-        return ans;
+        return {startIdx, endIdx};
     }
 };
