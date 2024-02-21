b2140df87b2b39f6bc73c8d12dc424938411cc6a
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-21 22:01:36 +0530

    Time: 61 ms (67.28%) | Memory: 34.5 MB (17.83%) - LeetSync

--- 1899-count-items-matching-a-rule/count-items-matching-a-rule.cpp
+++ 1899-count-items-matching-a-rule/count-items-matching-a-rule.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        int keyIndex = 0;
-    if (ruleKey == "color") {
-        keyIndex = 1;
-    } else if (ruleKey == "name") {
-        keyIndex = 2;
-    }
-    
-    return std::count_if(items.begin(), items.end(), [&](const auto& item) {
-        return item[keyIndex] == ruleValue;
-        });
+        int count=0;
+        int keyIndex=0;
+        if(ruleKey == "color")
+        {                          //  0      1       2
+            keyIndex=1; //set index  [typei, colori, namei]  this is a row
+        } 
+        else if(ruleKey == "name")
+        {
+            keyIndex=2;
+        }
+              
+        for(size_t i=0;i<items.size();i++)
+        {
+            if(items[i][keyIndex]==ruleValue)
+                count++;
+        }
+         return count;
     }
 };
