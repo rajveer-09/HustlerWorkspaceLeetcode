e2eb443ea22347e187f20dad62a366c4db521f98
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-23 18:48:24 +0530

    Time: 39 ms (99.90%) | Memory: 71.1 MB (22.58%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
-        int n=arr.size(),temp,mx=-1;
-        for(int i=n-1;i>=0;i--){
-            temp=arr[i];
-            arr[i]=mx;
-            mx=max(mx,temp);
+        int m=0,j=-1,len=arr.size()-1;
+        for(int i=len;i>=0;i--)
+        {
+            m=max(arr[i],m);
+            arr[i]=j;
+            j=m;
         }
         return arr;
     }
 };
+
+static const int _ = []() {
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    return 0;
+}();
