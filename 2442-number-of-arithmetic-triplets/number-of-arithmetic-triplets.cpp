505d43c4ed6c81596c411cf40665b3c9902b1502
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-23 11:55:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (52.96%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
-        int n=nums.size();
-        int cnt=0;
+        int cnt = 0;
+        int n = nums.size();
+
         for (int i = 0; i < n - 2; ++i) {
-            for (int j = i + 1; j < n - 1; ++j) {
-                for (int k = j + 1; k < n; ++k) {
-                    if (nums[j] - nums[i] == diff && nums[k] - nums[j] == diff) {
-                        cnt++;
-                    }
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int d = nums[j] - nums[i];
+                if (d == diff && nums[k] - nums[j] == diff) {
+                    cnt++;
+                    j++;
+                    k--;
+                } else if (d < diff) {
+                    j++;
+                } else {
+                    k--;
                 }
             }
         }
