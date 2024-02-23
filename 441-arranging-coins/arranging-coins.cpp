aeda1d7e3e950c474b2f9c2a2327ac745c4af7cf
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-23 11:21:14 +0530

    Time: 3 ms (68.01%) | Memory: 7.5 MB (47.64%) - LeetSync

--- 441-arranging-coins/arranging-coins.cpp
+++ 441-arranging-coins/arranging-coins.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int ans=0;
-        int i=1;
-        while(n){
-            if(n<i) break;
-            n=n-i;
-            ans=i;
-            i++;  
+        long int sum=0;int i;
+        for(i=1;sum<=n;i++){
+            sum+=i;
         }
-        return ans;  
+        return i-2;
     }
 };
