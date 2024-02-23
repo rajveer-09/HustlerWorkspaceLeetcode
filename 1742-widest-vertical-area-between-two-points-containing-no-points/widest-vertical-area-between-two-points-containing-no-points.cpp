703363161d1320267f3f56ae72d57f5718f69657
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-23 12:10:27 +0530

    Time: 112 ms (99.95%) | Memory: 70.3 MB (27.53%) - LeetSync

--- 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
+++ 1742-widest-vertical-area-between-two-points-containing-no-points/widest-vertical-area-between-two-points-containing-no-points.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
+    Solution()
+    {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+    }
+
     int maxWidthOfVerticalArea(vector<vector<int>>& points) {
-        int n = points.size();
-        
-        sort(points.begin(), points.end());
-        int maxArea = 0;
-        for(int i=1;i<n;i++) {
-            int currArea = points[i][0]-points[i-1][0];
-            maxArea = max(maxArea, currArea);
-        }
-        return maxArea;
+        vector<int> x_coords;
+        // x_coords.reserve(points.size());
+        for (const auto& p : points)
+            x_coords.push_back(p[0]);
+
+        sort(x_coords.begin(), x_coords.end());
+        int max_val = numeric_limits<int>::min();
+        for (int i = 1; i < x_coords.size(); i++)
+            max_val = max(max_val, x_coords[i] - x_coords[i-1]);
+
+        return max_val;
     }
 };
