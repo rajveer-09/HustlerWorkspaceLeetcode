ef296426c82e4f194de27992e40d0a20ea4c1f7f
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-24 20:54:21 +0530

    Time: 4 ms (63.27%) | Memory: 12.1 MB (46.56%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans(2*n);
+        vector<int> ans(2*n,0);
         int st = 0, en = n;
         for (int i = 0; i < n; i++) {
             ans[st++] = nums[i];
