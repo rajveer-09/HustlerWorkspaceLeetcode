b11194c8c75c17344a7a56aa8efc240b499c8632
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-24 10:56:04 +0530

    Time: 4 ms (80.66%) | Memory: 8.5 MB (57.73%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-       int start=0;
-       int end=s.size()-1;
-       while(start<=end){
-           if(!isalnum(s[start])){start++; continue;}
-           if(!isalnum(s[end])){end--;continue;}
-           if(tolower(s[start])!=tolower(s[end]))return false;
-           else{
-               start++;
-               end--;
-           }
-       }
-       return true;
+        int i = 0;
+        int j = s.size() - 1;
+        while (j > i){
+            if (!isalnum(s[j])){
+                j --;
+            }
+            else if (!isalnum(s[i])){
+                i ++ ;
+            }
+            else if (tolower(s[i++]) != tolower(s[j--])){
+                return false;
+            }
+        }
+
+        return true;
     }
 };
