ec90675bca59bdac59ea4b8397986d1e2258db22
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-02-24 21:16:56 +0530

    Time: 4 ms (36.64%) | Memory: 9.9 MB (13.34%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
         int sum=0;
-        int max_1=0;
+        int max_sum=0;
         vector<int> ans;
         for(int i=0;i<gain.size();i++){
             sum=sum+gain[i];
-            max_1=max(max_1,sum);
+            max_sum=max(max_sum,sum);
         }
-        return max_1;
+        return max_sum;
     }
 };
