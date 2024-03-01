43dc38f40be8628bffad410308d97cb7e089255a
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-01 23:37:16 +0530

    Time: 11 ms (46.09%) | Memory: 45.5 MB (31.00%) - LeetSync

--- 2502-sort-the-people/sort-the-people.java
+++ 2502-sort-the-people/sort-the-people.java
@@ -1,10 +1,8 @@
 class Solution {
     public String[] sortPeople(String[] names, int[] heights) {
         int n = names.length;
-
         for (int i = 0; i < n - 1; i++) {
             int maxIndex = i;
-
             for (int j = i + 1; j < n; j++) {
                 if (heights[j] > heights[maxIndex]) {
                     maxIndex = j;
