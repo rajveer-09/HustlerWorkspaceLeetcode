55e6d1a15271496c6ced7d4d823ad04fa46c085b
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-07 00:00:42 +0530

    Time: 56 ms (63.55%) | Memory: 31.9 MB (28.50%) - LeetSync

--- 436-find-right-interval/find-right-interval.cpp
+++ 436-find-right-interval/find-right-interval.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     vector<int> findRightInterval(vector<vector<int>>& intervals) {
-        map<int, int> startPoints;
-        vector<int> ans;
+         vector<int> ans;
+        map<int, int> m { {INT_MAX, -1} };
 
-        for (int i = 0; i < intervals.size(); i++) {
-            startPoints[intervals[i][0]] = i;
+        for(int i=0; i<intervals.size(); i++)
+            m[intervals[i][0]] = i;
+
+       for (int i = 0; i < intervals.size(); i++) {
+            int endOfCurrentInterval = intervals[i][1];
+            auto it = m.lower_bound(endOfCurrentInterval);
+            ans.push_back(it->second);
         }
-
-        for (auto& interval : intervals) {
-            auto it = startPoints.lower_bound(interval[1]);
-            if (it != startPoints.end()) {
-                ans.push_back(it->second);
-            } else {
-                ans.push_back(-1);
-            }
-        }
-
         return ans;
     }
 };
