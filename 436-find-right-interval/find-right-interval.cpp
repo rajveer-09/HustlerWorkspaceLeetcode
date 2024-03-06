f1fb771b96a2a6fafad85cf36321c80d723e9a4b
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-07 00:04:29 +0530

    Time: 31 ms (99.50%) | Memory: 28.6 MB (76.64%) - LeetSync

--- 436-find-right-interval/find-right-interval.cpp
+++ 436-find-right-interval/find-right-interval.cpp
@@ -8,7 +8,6 @@ public:
             startPoints.push_back({intervals[i][0], i});
         }
         sort(startPoints.begin(), startPoints.end());
-
         for (int i = 0; i < intervals.size(); i++) {
             auto it = lower_bound(startPoints.begin(), startPoints.end(), make_pair(intervals[i][1], -1));
             if (it != startPoints.end()) {
