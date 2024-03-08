975eccf6328df66bfe0880bcd4637cb14c888016
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-08 11:19:11 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (84.51%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -1,20 +1,19 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
-        Arrays.sort(nums);
+        int[] frequency = new int[201];
         int sum = 0;
-        int n = nums.length;
-        
-        if (n == 1 || nums[0] != nums[1]) 
-            sum += nums[0];
-        
-        for (int i = 1; i < n - 1; i++) {
-            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) 
-                sum += nums[i];
+
+        for (int num : nums) {
+            frequency[num + 100]++;
         }
-        
-        if (n >= 2 && nums[n - 1] != nums[n - 2]) 
-            sum += nums[n - 1];
-        
+
+        for (int i = 0; i < 201; i++) {
+            if (frequency[i] == 1) {
+                sum += i - 100;
+            }
+        }
+
         return sum;
     }
 }
+
