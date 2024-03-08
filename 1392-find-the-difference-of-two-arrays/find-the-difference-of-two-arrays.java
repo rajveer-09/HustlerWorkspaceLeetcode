65cad7d5be7aabd16ce18b5dc4eafdb525ddbba4
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-08 11:36:32 +0530

    Time: 7 ms (97.56%) | Memory: 45.6 MB (24.77%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
@@ -7,15 +5,14 @@ class Solution {
         
         List<Integer> diff1 = new ArrayList<>();
         List<Integer> diff2 = new ArrayList<>();
-        
         int i = 0, j = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
+        while (i < nums1.length || j < nums2.length) {
+            if (i < nums1.length && (j == nums2.length || nums1[i] < nums2[j])) {
                 if (i == 0 || nums1[i] != nums1[i - 1]) {
                     diff1.add(nums1[i]);
                 }
                 i++;
-            } else if (nums1[i] > nums2[j]) {
+            } else if (j < nums2.length && (i == nums1.length || nums2[j] < nums1[i])) {
                 if (j == 0 || nums2[j] != nums2[j - 1]) {
                     diff2.add(nums2[j]);
                 }
@@ -25,21 +22,6 @@ class Solution {
                 j++;
             }
         }
-        
-        while (i < nums1.length) {
-            if (i == 0 || nums1[i] != nums1[i - 1]) {
-                diff1.add(nums1[i]);
-            }
-            i++;
-        }
-        
-        while (j < nums2.length) {
-            if (j == 0 || nums2[j] != nums2[j - 1]) {
-                diff2.add(nums2[j]);
-            }
-            j++;
-        }
-        
         List<List<Integer>> result = new ArrayList<>();
         result.add(diff1);
         result.add(diff2);
@@ -47,3 +29,4 @@ class Solution {
         return result;
     }
 }
+
