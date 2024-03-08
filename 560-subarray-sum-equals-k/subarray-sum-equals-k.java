1db8ee535dd5ce4e2cae13f2159f59ce29ae40b2
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-08 17:51:57 +0530

    Time: 1111 ms (11.80%) | Memory: 45.9 MB (86.89%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,15 +1,24 @@
 public class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
+        int[] prefixSum = new int[nums.length + 1];
+        prefixSum[0] = 0;
+
+        // Calculate prefix sum
+        for (int i = 1; i <= nums.length; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        }
+
+        // Check subarrays sum
         for (int start = 0; start < nums.length; start++) {
-            int sum = 0;
-            for (int end = start; end < nums.length; end++) {
-                sum += nums[end];
+            for (int end = start + 1; end <= nums.length; end++) {
+                int sum = prefixSum[end] - prefixSum[start];
                 if (sum == k) {
                     count++;
                 }
             }
         }
+
         return count;
     }
 }
