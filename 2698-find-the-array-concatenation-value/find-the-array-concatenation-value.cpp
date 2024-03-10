7af2f64922afee993ef6f819ce8a01b2fec983d7
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-10 12:41:17 +0530

    Time: 4 ms (68.28%) | Memory: 12.7 MB (25.05%) - LeetSync

--- 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.cpp
+++ 2698-find-the-array-concatenation-value/find-the-array-concatenation-value.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     long long findTheArrayConcVal(vector<int>& nums) {
-        int i = 0,j=nums.size()-1,x,y;
-        long long int ans=0;
-        while(i<j){
-            x = nums[j];
-            y = 0;
-            while(x){
-                y++;
-                x /= 10;
-            }
-            ans += (nums[i]*1LL*pow(10,y)+nums[j]);
-            i++;
-            j--;
+        int n=nums.size();
+        int l=0, r=n-1 ;
+        if(n==1)
+        return nums[0];
+        long long conct=0;
+        string str="";
+        while(l<r)
+        {
+            str+=to_string(nums[l]);
+            str+=to_string(nums[r]);
+            conct+=stoi(str);
+            str="";
+            l++;r--;
+            if(l==r)
+            conct+=nums[l];
         }
-        if(i==j){
-            ans += nums[i];
-        }
-        return ans;
+        return conct;
     }
 };
