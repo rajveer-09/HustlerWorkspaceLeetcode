8de4b434d04e994c979b486bf595666d25829e17
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-10 12:25:48 +0530

    Time: 16 ms (41.57%) | Memory: 23.7 MB (73.22%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -6,7 +6,7 @@ public:
         int j=i+1;
         while(i<n && j<n){
             if((nums[i]&1) ==0) i+=2;
-            if((nums[j]&1) !=0) j+=2;
+            else if((nums[j]&1) !=0) j+=2;
             else{
                 swap(nums[i],nums[j]);
             }
