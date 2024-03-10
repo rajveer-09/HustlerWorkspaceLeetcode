fe43d9ac3634a5ab2c5fea2ae15d654c1a82b6bd
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-10 23:05:30 +0530

    Time: 17 ms (43.18%) | Memory: 16.4 MB (35.13%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
     vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
-        sort(nums.begin(),nums.end());
-        vector<int> ans(queries.size(),0);
-        int n=nums.size(),m=queries.size();
-        for(int i=0;i<m;i++){
-            int sum=0;
-            int count=0;
-            for(int j=0;j<n;j++){
-                sum+=nums[j];
-                if(sum>queries[i]) break;
-                count++;
+        sort(nums.begin(), nums.end());
+        vector<int> ans(queries.size(), 0);
+        int n = nums.size(), m = queries.size();
+        vector<int> temp(nums.size(), 0);
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            temp[i] = sum;
+        }
+        for (int i = 0; i < m; i++) {
+            int count = 0;
+            int flg = false;
+            int st = 0, ed = n - 1;
+            while (st <= ed) {
+                int mid = st + (ed - st) / 2;
+                if (temp[mid] == queries[i]) {
+                    count = mid;
+                    flg = true;
+                    st = mid + 1;
+                }
+                else if (temp[mid] < queries[i]) {
+                    st = mid + 1;
+                }
+                else {
+                    ed = mid - 1;
+                }
             }
-            ans[i]=count;
+            if (flg) {
+                ans[i] = st;
+            } else {
+                ans[i] = st;
+            }
         }
         return ans;
     }
