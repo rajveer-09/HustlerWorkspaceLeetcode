5dcedeae3f409b0d2741a252c48ddf4e497cf6a2
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-11 12:46:08 +0530

    Time: 52 ms (5.68%) | Memory: 25.6 MB (13.61%) - LeetSync

--- 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.cpp
@@ -1,32 +1,32 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int smallestDivisor(std::vector<int>& nums, int threshold) {
-        int left = 1;
-        int right = *std::max_element(nums.begin(), nums.end());
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int sum = calculateSum(nums, mid);
-            
-            if (sum > threshold) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        
-        return left;
+    int round(double a,double b){
+        int x=a/b;
+        double n=a/b;
+        if(n-x>0)
+            return x+1;
+        return x;
     }
-    
-private:
-    int calculateSum(const std::vector<int>& nums, int divisor) {
-        int sum = 0;
-        for (int num : nums) {
-            sum += (num + divisor - 1) / divisor;
+
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        long long int s=1,e,ans;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();++i)
+            e=e+nums[i];
+        long long int mid=s+(e-s)/2;
+        while(s<=e){
+            int sum=0;
+            for(int i=0;i<nums.size();++i){
+                sum+= (round(nums[i],mid));
+            }
+            if(sum<=threshold){
+                e=mid-1;
+                ans=mid;
+            }
+            else
+                s=mid+1;
+            mid=s+(e-s)/2;
         }
-        return sum;
+        return mid;
     }
 };
