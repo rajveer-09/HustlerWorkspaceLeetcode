f75fac15ab20d6479fe45aac12bc1029d519e9cf
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-16 02:50:52 +0530

    Time: 4 ms (71.70%) | Memory: 12.5 MB (79.02%) - LeetSync

--- 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-        bool flag = true;
-        for (int i = 0; i < nums.size();++i){
-            if (nums[i] == 0)
-                return 0;
-            if (nums[i] < 0)
-                flag = !flag;
+        int p=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0) return 0;
+            else if(nums[i]<0) p*=-1;
         }
-        if (flag)
-            return 1;
-        return -1;
+        return p;
     }
 };
