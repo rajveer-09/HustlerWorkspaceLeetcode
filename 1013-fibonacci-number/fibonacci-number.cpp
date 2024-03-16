3c4b41719238f4c887980d9cec66bcf1f23fdcf4
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-16 17:04:43 +0530

    Time: 3 ms (65.50%) | Memory: 7.6 MB (11.46%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,24 @@
+#include <unordered_map>
+
 class Solution {
 public:
-
     int fib(int n) {
-        if(n==0){
+        std::unordered_map<int, int> memo;
+        return fibMemo(n, memo);
+    }
+
+private:
+    int fibMemo(int n, std::unordered_map<int, int>& memo) {
+        if (n == 0) {
             return 0;
         }
-        else if(n==1){
+        if (n == 1) {
             return 1;
         }
-        return fib(n-1)+fib(n-2);
+        if (memo.find(n) != memo.end()) {
+            return memo[n];
+        }
+        memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+        return memo[n];
     }
 };
