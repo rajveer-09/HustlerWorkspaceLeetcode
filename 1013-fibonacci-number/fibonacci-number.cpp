cbb4e075edcd6d1f902cb9e8185b1e7ae5e4c002
Rajveer Sharma <143857709+rajveer-09@users.noreply.github.com>
2024-03-16 17:01:41 +0530

    Time: 3 ms (65.50%) | Memory: 7.2 MB (11.46%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,20 @@
-class Solution {
+class Solution{
 public:
-    int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-
-        return fib(n-1)+fib(n-2);
+    int fib(int n){
+        int a=0, b=1;
+        int ans=0;
+        if(n==0){
+            return a;
+        }
+        if(n==1){
+            return b;
+        }
+        for(int i=2;i<=n;i++){
+            int c=a+b;
+            a=b;
+            b=c;
+            ans=c;
+        }   
+        return ans;
     }
 };
