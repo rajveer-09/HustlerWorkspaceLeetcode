21072442a9d9a31914ec7f088899e64eb38978d5
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-04 16:05:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (99.62%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int arr[256]={0};
-
-        for(int i=0;i<s.size();i++){
-            arr[s[i]]++;
+        int arr1[26]{0};
+        int arr2[26]{0};
+        for(char ch : s){
+            if(ch>='a' && ch<='z') arr1[ch-'a']++;
+            else arr2[ch-'A']++;
         }
-        int cnt =0;
-        bool flag=false;
-        for(int i=0;i<256;i++){
-            if(!(arr[i]&1)) cnt+=arr[i];
-            else{
-                cnt+=arr[i]-1;
-                flag=true;
-            }
+        int ans=0;
+        int flag=0;
+        for(int i=0;i<26;i++){
+            if(arr1[i]%2==0) ans+=arr1[i];
+            else if(arr1[i]%2!=0) ans+=arr1[i]-1,flag=1;
+            if(arr2[i]%2==0) ans+=arr2[i];
+            else if(arr2[i]%2!=0) ans+=arr2[i]-1,flag=1;
         }
-        if(flag) cnt++;
-        return cnt;
+        if(flag==1) ans++;
+        return ans;
     }
 };
