5acb7272795270bcfe42f1b966040daceb5723e2
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-13 18:58:13 +0530

    Time: 2 ms (77.65%) | Memory: 7 MB (93.09%) - LeetSync

---
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        int winner = 0; // Start with the base case for 1 person
+        for (int i = 2; i <= n; ++i) {
+            winner = (winner + k) % i;
+        }
+        return winner + 1; // Convert zero-based index to one-based index
+    }
+};
