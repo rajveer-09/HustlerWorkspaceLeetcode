494d58ce335a972ec5331eef406c9735b80828c5
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-23 01:36:11 +0530

    Time: 4 ms (33.13%) | Memory: 7.9 MB (47.45%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,19 +1,18 @@
+#include <vector>
+
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> generate(int n) {
         vector<vector<int>> ans(n);
 
-        if (n >= 1) ans[0] = {1};       
-        if (n >= 2) ans[1] = {1, 1};
-
-        for (int i = 2; i < n; ++i) {
-            vector<int> temp(i + 1, 0);
-            temp[0] = 1;  
+        for (int i = 0; i < n; ++i) {
+            ans[i].resize(i + 1);
+            ans[i][0] = ans[i][i] = 1;
             for (int j = 1; j < i; ++j) {
-                temp[j] = ans[i - 1][j - 1] + ans[i - 1][j];
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
             }
-            temp[i] = 1;
-            ans[i] = temp;
         }
 
         return ans;
