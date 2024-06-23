45f9dfb7e596a141ec3f33b0a4b895d8dd8e2646
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-23 17:19:33 +0530

    Time: 27 ms (34.07%) | Memory: 22.8 MB (30.04%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        if (n == 0) return {};
         
-      if(intervals.size()==1)
-         return intervals;
-      vector<pair<int,int>> p;
-      for(int i=0;i<intervals.size();i++)
-      {
-          p.push_back({intervals[i][0],intervals[i][1]});
-      } 
-      sort(p.begin(),p.end());
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
 
-      vector<vector<int>> ans;
-      int f=p[0].first,s=p[0].second;
-      for(int i=0;i<p.size()-1;i++)
-      {
-          vector<int> a(2);
-          if(s>=p[i+1].first)
-          {
-              s=max(s,p[i+1].second);
-          }
-          else
-          {
-              a[0]=f;
-              a[1]=s;
-              f=p[i+1].first;
-              s=p[i+1].second;
-              ans.push_back(a);
-          }
-      } 
-      int n=intervals.size();
-      ans.push_back({f,s});
-      return ans;
+        int mini = intervals[0][0];
+        int maxi = intervals[0][1];
+        
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] <= maxi) {
+                if (intervals[i][1] > maxi) {
+                    maxi = intervals[i][1];
+                }
+            } else {
+                ans.push_back({mini, maxi});
+                mini = intervals[i][0];
+                maxi = intervals[i][1];
+            }
+        }
+        
+        ans.push_back({mini, maxi});
+        
+        return ans;
     }
 };
