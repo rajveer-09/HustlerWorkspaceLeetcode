99fc927ddb4aaeaf4357ce1c1cb4ceb7aaf522c7
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-25 04:20:24 +0530

    Time: 212 ms (22.26%) | Memory: 252.3 MB (11.68%) - LeetSync

---
+++ 2867-ways-to-split-array-into-good-subarrays/ways-to-split-array-into-good-subarrays.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    #define ll long long
+    const int MOD = 1e9 + 7;
+    
+    //Idea : to find number of ways the number of Zeroes present between two adjacent Ones.
+
+    int numberOfGoodSubarraySplits(vector<int>& nums) {
+        vector<ll> vec;
+        int s = nums.size();
+        
+        //Step 1 - store the position of ones 
+        for(int i = 0; i < s; i++){
+            if(nums[i] == 1){
+                vec.push_back(i);
+            }
+        }
+        //when no ones present 
+        if(vec.size() == 0){
+            return 0;
+        }
+        
+        ll ans = 1;
+        //simply take the absolute difference of position and multiply to the ans
+        //also take mod at each step
+        for(int i = 1; i < vec.size(); i++){
+            ans = ((ans%MOD) * ((vec[i - 1] - vec[i])%MOD)) % MOD;
+        }
+        
+        return abs(ans % MOD);
+    }
+};
