5d5829d3b6aea9ef8c03598fd6efb27dd167fa94
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-25 04:06:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (46.76%) - LeetSync

--- 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
+++ 2837-minimum-operations-to-make-the-integer-zero/minimum-operations-to-make-the-integer-zero.cpp
@@ -1,14 +1,32 @@
-#define ll long long
 class Solution {
 public:
-    int makeTheIntegerZero(int num1, int num2) {
-        //num1 = k*num2 + (2^i1 + 2^i2 + .... + 2^1k)
-        //num1 - k*num2 = (2^i1 + 2^i2 + .... + 2^1k)
-        for(ll i = 0 ; i <= 60 ; i++){
-            ll required = num1 - i*num2;
-            ll setbit = __builtin_popcountll(required);
-            if(setbit <= i && i <= required) return i;
+    int count_bits(long long int num){
+        int count = 0;
+        while(num){
+            if(num&1){
+                count++;
+            }
+            num=num>>1;
         }
-        return -1;
+        return count;
+    }
+
+    int makeTheIntegerZero(int num1, int num2) {
+       
+       if(num1<=num2)return -1;
+       
+       long long int k=0;
+       while(num1-k*num2>0)
+       {
+           long long int diff = num1 - k*num2 ;
+           long long int bitCount = count_bits(diff);
+           
+           if(k<=diff && k>=bitCount)
+            return k;
+
+            k++;
+       }
+
+       return -1;
     }
 };
