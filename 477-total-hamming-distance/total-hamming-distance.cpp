e69a54de2b3296881859c3555077ebd9c35bd6aa
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-27 02:23:35 +0530

    Time: 14 ms (99.67%) | Memory: 21.5 MB (18.50%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-
+    Solution(){
+        ios::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+    }
     int totalHammingDistance(vector<int>& nums) {
         int res = 0, n = nums.size();
         for(int i = 0 ; i < 32 ; i++){
-            int cnt_bit1 = 0;
+            int number_of_bit_1 = 0;
             for(int num : nums){
-                cnt_bit1 += (num>>i) & 1; 
+                number_of_bit_1 += (num>>i) & 1; 
             }
-            res += cnt_bit1 * (n-cnt_bit1);
+            res += number_of_bit_1 * (n-number_of_bit_1);
         }
         return res;
     }
