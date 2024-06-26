9fea535fc7e8084ff141fb356f2d02d0211b01df
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-27 02:41:58 +0530

    Time: 85 ms (59.71%) | Memory: 138.8 MB (24.40%) - LeetSync

--- 464-can-i-win/can-i-win.cpp
+++ 464-can-i-win/can-i-win.cpp
@@ -1,48 +1,23 @@
 class Solution {
-public:
-    
-
-bool canWin(int maxChoosableInteger, int desiredTotal, int usedNumbers, unordered_map<int, bool>& memo) {
-    if (desiredTotal <= 0) return false;
-    if (memo.find(usedNumbers) != memo.end()) return memo[usedNumbers];
-
-    for (int i = 0; i < maxChoosableInteger; ++i) {
-        int currentMask = (1 << i);
-        if ((usedNumbers & currentMask) == 0) { // If i-th number is not used
-            if (!canWin(maxChoosableInteger, desiredTotal - (i + 1), usedNumbers | currentMask, memo)) {
-                memo[usedNumbers] = true;
-                return true;
-            }
+private:
+    int m,t;
+    vector<vector<int>> dp;
+    bool game(int mask,int player,int score){
+        if(dp[player][mask]!=-1) return dp[player][mask];
+        for(int i=0;i<m;i++)
+        {
+            int cmask = 1<<i;
+            if( (mask&cmask) == 0 )
+                    if(score+i+1 >= t or game((mask|cmask),(player^1),score+i+1)==false ) return dp[player][mask]=1;
         }
+        return dp[player][mask]=0;
     }
-
-    memo[usedNumbers] = false;
-    return false;
-}
-
-bool canIWin(int maxChoosableInteger, int desiredTotal) {
-    if (desiredTotal <= 0) return true;
-    int sum = (maxChoosableInteger * (maxChoosableInteger + 1)) / 2;
-    if (sum < desiredTotal) return false;
-
-    unordered_map<int, bool> memo;
-    return canWin(maxChoosableInteger, desiredTotal, 0, memo);
-}
-
-int main() {
-    int maxChoosableInteger = 10;
-    int desiredTotal = 11;
-    cout << (canIWin(maxChoosableInteger, desiredTotal) ? "true" : "false") << endl;
-
-    maxChoosableInteger = 10;
-    desiredTotal = 0;
-    cout << (canIWin(maxChoosableInteger, desiredTotal) ? "true" : "false") << endl;
-
-    maxChoosableInteger = 10;
-    desiredTotal = 1;
-    cout << (canIWin(maxChoosableInteger, desiredTotal) ? "true" : "false") << endl;
-
-    return 0;
-}
-
+public:
+    bool canIWin(int maxChoosableInteger, int desiredTotal) {
+        m=maxChoosableInteger;
+        t=desiredTotal;
+        if((m*(m+1))/2 < t) return false;
+        dp.resize(2,vector<int>(1<<m,-1));
+        return game(0,0,0);
+    }
 };
