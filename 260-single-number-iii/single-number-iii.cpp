3da124d07787566a4fc42f742eeb44f7b542d1ab
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-06-27 18:27:36 +0530

    Time: 3 ms (96.01%) | Memory: 12.5 MB (33.95%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        vector<int> result(2, 0);
-        int index = 0;
-        int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            bool found = false;
-            for (int j = 0; j < n; j++) {
-                if (i != j && nums[i] == nums[j]) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                result[index++] = nums[i];
-                if (index == 2) {
-                    break;
-                }
+        int xorResult = 0;
+        
+        // Step 1: Calculate XOR of all elements in nums
+        for (int num : nums) {
+            xorResult ^= num;
+        }
+        
+        // Step 2: Find the rightmost set bit in xorResult
+        int rightmostSetBit = 1;
+        while ((rightmostSetBit & xorResult) == 0) {
+            rightmostSetBit <<= 1;
+        }
+        
+        // Step 3: Initialize variables to store the two unique numbers
+        int num1 = 0, num2 = 0;
+        
+        // Step 4: Partition nums based on the rightmost set bit and find the two numbers
+        for (int num : nums) {
+            if (num & rightmostSetBit) {
+                num1 ^= num;
+            } else {
+                num2 ^= num;
             }
         }
-
-        return result;
+        
+        // Step 5: Return the two numbers in any order
+        return {num1, num2};
     }
 };
