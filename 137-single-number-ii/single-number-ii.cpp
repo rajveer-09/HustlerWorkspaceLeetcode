02867cb582d2ce29185a4713180a08702e54f1f7
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-07-01 14:48:32 +0530

    Time: 7 ms (50.89%) | Memory: 11.8 MB (81.95%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,14 +1,20 @@
 class Solution {
- public:
-  int singleNumber(vector<int>& nums) {
-    int ones = 0;
-    int twos = 0;
-
-    for (const int num : nums) {
-      ones ^= (num & ~twos);
-      twos ^= (num & ~ones);
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        
+        for(int i = 0; i < 32; i++) {
+            int temp = (1 << i);
+            int countOne = 0;
+            for(int num : nums) {
+                if((num & temp) != 0) {
+                    countOne++;
+                }
+            }
+            if(countOne % 3 == 1) {
+                result |= temp;
+            }
+        }
+        return result;
     }
-
-    return ones;
-  }
 };
