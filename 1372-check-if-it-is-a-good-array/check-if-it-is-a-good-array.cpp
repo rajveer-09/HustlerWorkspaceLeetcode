c5d0bc66c3480ca359cfa70950bbb980611838c5
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-07-03 11:20:51 +0530

    Time: 22 ms (96.69%) | Memory: 32.5 MB (5.42%) - LeetSync

--- 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.cpp
+++ 1372-check-if-it-is-a-good-array/check-if-it-is-a-good-array.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isGoodArray(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         int g = 0;
         for(auto &i: nums){
             g = __gcd(i,g);
