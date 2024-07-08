e869c5c71cc8af9f86a4b2bb364e97d8aededcbf
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-07-08 21:22:43 +0530

    Time: 3 ms (65.36%) | Memory: 7.1 MB (92.77%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        return josephus(n, k) + 1; // Add 1 to convert zero-based index to one-based index
+        return josephus(n, k) + 1; 
     }
 
 private:
     int josephus(int n, int k) {
         if (n == 1) {
-            return 0; // Base case: the last person standing is at index 0
+            return 0; 
         } else {
             return (josephus(n - 1, k) + k) % n;
         }
