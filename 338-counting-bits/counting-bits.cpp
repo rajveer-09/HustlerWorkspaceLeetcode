a501d697e9a66fe9d9c01f8ce312315b2b06dfd0
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-07-13 00:23:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (43.21%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans;
+        vector<int> ans(n + 1, 0);
 
-        for(int i=0;i<=n;i++){
-            int a=__builtin_popcount(i);
-            ans.push_back(a);
+        for (int i = 1; i <= n; ++i) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
 
         return ans;
