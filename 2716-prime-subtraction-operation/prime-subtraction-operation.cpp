7c877988178f0c19379675ccfd0f0eecb5a2ae28
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-08-12 01:43:03 +0530

    Time: 41 ms (22.46%) | Memory: 26.8 MB (88.56%) - LeetSync

--- 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -1,29 +1,44 @@
 class Solution {
 public:
     bool isPrime(int n) {
-        if(n <= 1) return false;
-        if(n <= 3) return true;
-        if(n % 2 == 0 || n % 3 == 0) return false;
-        for(int i = 5; i * i <= n; i += 6) {
-            if(n % i == 0 || n % (i + 2) == 0) return false;
+        if (n <= 1) {
+            return false;
+        } else if (n <= 3) {
+            return true;
+        } else if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        } else {
+            for (int i = 5; i * i <= n; i += 6) {
+                if (n % i == 0 || n % (i + 2) == 0) {
+                    return false;
+                }
+            }
+            return true;
         }
-        return true;
     }
-
     bool primeSubOperation(vector<int>& nums) {
         int n = nums.size();
-        for(int i = 0; i < n; ++i) {
-            int prev = (i > 0) ? nums[i-1] : 0;
-            for(int j = nums[i] - 1; j >= 2; --j) {
-                if(isPrime(j) && nums[i] - j > prev) {
-                    nums[i] -= j;
-                    break;
+        for (int i = 0; i < n; ++i) {
+            int prev;
+            if (i > 0) {
+                prev = nums[i - 1];
+            } 
+            else {
+                prev = 0;
+            }
+            for (int j = nums[i] - 1; j >= 2; --j) {
+                if (isPrime(j)) {
+                    if (nums[i] - j > prev) {
+                        nums[i] -= j;
+                        break;
+                    }
                 }
             }
         }
-        
-        for(int i = 1; i < n; ++i) {
-            if(nums[i] <= nums[i-1]) return false;
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] <= nums[i - 1]) {
+                return false;
+            }
         }
         return true;
     }
