c512f196e7c9fed66b8704cfb0a91f1df78978bc
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-08-22 06:19:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (7.20%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
-    int numWaterBottles(int n, int e) {
-        int total = n;
-
-        while (n >= e) {
-            total += n / e;
-            n = (n / e) + (n % e);
-        }
-
-        return total;
+    int numWaterBottles(int nB, int nE) {
+        return nB + (nB-1)/(nE-1);
     }
 };
