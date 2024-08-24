03fb82d79b15ee186315f3448183e79d345bd568
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-08-24 16:57:47 +0530

    Time: 18 ms (81.19%) | Memory: 12.8 MB (5.11%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.cpp
@@ -1,23 +1,39 @@
 class Solution {
-public:
-    int distributeCookies(vector<int>& cookies, int k) {
-        vector<int> child(k, 0); // Track cookies given to each child
-        return solve(0, cookies, child, k);
+public:    
+    int solve(vector<int>& cookies, int mask, int k, vector<vector<int>>& dp) {
+        if (dp[mask][k] > 0)
+            return dp[mask][k];
+        if (k == 1) {
+            // last person gets the rest
+            int unfairness = 0;
+            for (int j = 0; j < cookies.size(); j++) {
+                if (mask & (1 << j))
+                    continue;
+                unfairness += cookies[j];
+            }
+            dp[mask][k] = unfairness;
+            return unfairness;
+        }
+        int ans = INT_MAX;
+        for (int i = 0; i < (1 << cookies.size()); i++) {
+            if (i & mask)
+                continue;
+            int unfairness = 0;
+            for (int j = 0; j < cookies.size(); j++) {
+                if (i & (1 << j)) {
+                    unfairness += cookies[j];
+                }
+            }
+            unfairness = max(unfairness, solve(cookies, mask | i, k - 1, dp));
+            ans = min(ans, unfairness);
+        }
+        dp[mask][k] = ans;
+        return ans;
     }
 
-private:
-    int solve(int idx, vector<int>& cookies, vector<int>& child, int k) {
-        if (idx == cookies.size()) {
-            return *max_element(child.begin(), child.end()); // Max cookies given to a single child
-        }
-
-        int minUnfairness = INT_MAX;
-        for (int i = 0; i < k; ++i) {
-            child[i] += cookies[idx]; // Give current cookie to i-th child
-            minUnfairness = min(minUnfairness, solve(idx + 1, cookies, child, k)); // Recurse for next cookie
-            child[i] -= cookies[idx]; // Backtrack
-        }
-
-        return minUnfairness;
+    int distributeCookies(vector<int>& cookies, int k) {
+        int len = cookies.size();
+        vector<vector<int>> dp(1024, vector<int>(10, -1));
+        return solve(cookies, 0, k, dp);
     }
 };
