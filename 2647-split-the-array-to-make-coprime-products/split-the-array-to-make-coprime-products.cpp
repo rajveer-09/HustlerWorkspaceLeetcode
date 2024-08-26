1bc343e28128dab6f78e65a9d6af224fc1b9d3d0
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-08-27 04:56:45 +0530

    Time: 59 ms (97.02%) | Memory: 36.6 MB (46.81%) - LeetSync

--- 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
+++ 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
@@ -1,29 +1,78 @@
+vector<long long> generatePrimes(long long n) {
+    vector<bool> isPrime(n + 1, true);
+    isPrime[0] = isPrime[1] = false;
+    for (long long i = 2; i * i <= n; ++i) {
+        if (isPrime[i]) {
+            for (long long j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    vector<long long> primes;
+    for (long long i = 2; i <= n; ++i) {
+        if (isPrime[i]) {
+            primes.push_back(i);
+        }
+    }
+    return primes;
+}
+
 class Solution {
 public:
-    long long int gcd(int a, int b) {
-        if (b == 0) {
-            return a;
-        }
-        return gcd(b, a % b);
-    }
-
     int findValidSplit(vector<int>& nums) {
-        int n = nums.size();
-        if (nums[0] == 707929)
-            return 4090; // test case failing
-        // if(n==1)return -1;
-        int i = 0, j = n - 1, ans = 0;
-        while (i <= ans) {
-            j = n - 1;
-            while (j > ans && gcd(nums[i], nums[j]) == 1) {
-                j--;
-            }
-            i++;
-            ans = j;
+        if (nums.size() <= 1) return -1;
 
-            if (ans == n - 1)
-                return -1;
+        unordered_map<long long, long long> minPos, maxPos;
+        vector<long long> primes = generatePrimes(1000);
+
+        auto updateFactorPositions = [&](long long n, long long idx) {
+            for (long long p : primes) {
+                if (p * p > n) break;
+                if (n % p == 0) {
+                    while (n % p == 0) n /= p;
+                    if (minPos.find(p) == minPos.end()) {
+                        minPos[p] = idx;
+                        maxPos[p] = idx;
+                    } else {
+                        maxPos[p] = idx;
+                    }
+                }
+            }
+            if (n > 1) {
+                if (minPos.find(n) == minPos.end()) {
+                    minPos[n] = idx;
+                    maxPos[n] = idx;
+                } else {
+                    maxPos[n] = idx;
+                }
+            }
+        };
+
+        for (long long i = 0; i < nums.size(); ++i) {
+            updateFactorPositions(nums[i], i);
         }
-        return ans;
+
+        vector<pair<long long, int>> events;
+        for (const auto& [factor, pos] : minPos) {
+            events.push_back({pos, 0});
+            events.push_back({maxPos[factor], 1});
+        }
+
+        sort(events.begin(), events.end());
+
+        if (events.empty() || events[0].first != 0) return 0;
+
+        int count = 0;
+        for (const auto& [pos, type] : events) {
+            if (type == 0) {
+                ++count;
+            } else {
+                --count;
+                if (count == 0 && pos + 1 < nums.size()) {
+                    return pos;
+                }
+            }
+        }
+        return -1;
     }
 };
