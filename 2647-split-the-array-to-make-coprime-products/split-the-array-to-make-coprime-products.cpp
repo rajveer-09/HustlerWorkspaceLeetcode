56d963e55c3297d54cb1a634b7a0cb5f2c7a303b
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-08-27 12:36:40 +0530

    Time: 32 ms (98.72%) | Memory: 31.2 MB (63.83%) - LeetSync

--- 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
+++ 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
@@ -1,60 +1,149 @@
+// class Solution {
+// public:
+//     int findValidSplit(vector<int>& nums) {
+//         long long n = nums.size();
+//         for (int i = 0; i <= n - 2; i++) {
+//             long long u = 1, v = 1;
+//             for (int j = 0; j <= i; j++) {
+//                 u *= nums[j];
+//             }
+//             for (int k = i + 1; k < n; k++) {
+//                 v *= nums[k];
+//             }
+//             if (__gcd(u, v) == 1) {
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+// };
+// #define f first
+// #define s second
+// class Primes {
+//   private:
+//     int n;
+//   public:
+//     vector<int> isPrime;
+//     vector<int> primeFactor;
+//     Primes (int n) {
+//       isPrime.resize(n + 1, 1);
+//       primeFactor.resize(n + 1);
+//       int i = 2;
+//       isPrime[0] = isPrime[1] = false;
+//       while(i * i <= n) {
+//         if (isPrime[i]) {
+//           for (int j = i * i; j <= n; j += i) {
+//             isPrime[j] = false;
+//             primeFactor[j] = i;
+//           }
+//         }
+//         i++;
+//       }
+//       for(int i = 2; i <= n; i++) {
+//         if (!isPrime[i]) continue;
+//         primeFactor[i] = i;
+//       }
+//     }
+// };
+
+// class Solution {
+// public:
+//     int findValidSplit(vector<int>& nums) {
+//         int maxEle = *max_element(nums.begin(), nums.end());
+//         Primes p(maxEle);
+//         int n = nums.size();
+//         map<int, pair<int, int>> pos;
+//         for (int i = 0; i < n; i++) {
+//             int val = nums[i];
+//             while (val != 1) {
+//                 int pf = p.primeFactor[val];
+//                 //storing start & end positions of a prime
+//                 if (pos.find(pf) == pos.end()) pos[pf] = {i, i};
+//                 else pos[pf].s = i;
+//                 while (val % pf == 0) {
+//                     val /= pf;
+//                 }
+//             }
+//         }
+//         vector<int> isNotPossible(n, 0);
+//         for (auto p: pos) {
+//             // making sure split is not possible between start and end
+//             isNotPossible[p.s.f] += 1;
+//             isNotPossible[p.s.s] += -1;
+//         }
+//         for (int i = 1; i < n; i++) {
+//             isNotPossible[i] += isNotPossible[i - 1];
+//         }
+//         for (int i = 0; i < n - 1; i++) if (!isNotPossible[i]) return i;
+//         return -1;
+//     }
+// };
+
+
+
+
+
+
+
+
+
+
+
+constexpr int MAX = 1000100;
+
+int a[MAX], p[MAX], l[MAX], r[MAX];
+
+void Prime() {
+  int n = 0;
+  for (int i = 2; i < MAX; ++i) {
+    if (!a[i]) {
+      a[i] = i;
+      p[n++] = i;
+    }
+    for (int j = 0; i * p[j] < MAX; ++j) {
+      a[i * p[j]] = p[j];
+      if (i % p[j] == 0) {
+        break;
+      }
+    }
+  }
+}
+
 class Solution {
 public:
-    void primeFreq(int x, unordered_map<int, int> &mp) {
-        for (int i = 2; i * i <= x; i++) {
-            while (x % i == 0) {
-                mp[i] += 1;
-                x /= i;
-            }
-        }
-        if (x > 1) {  // If x is a prime number
-            mp[x] += 1;
-        }
-    }
-
     int findValidSplit(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
+      static bool init = []() {
+        Prime();
+        return true;
+      }();
 
-        // Global frequency of prime factors
-        unordered_map<int, int> global_freq;
-        vector<unordered_map<int, int>> prime_factors(nums.size());
-
-        // Calculate prime factor frequencies for each element and overall frequency
-        for (int i = 0; i < nums.size(); i++) {
-            primeFreq(nums[i], prime_factors[i]);
-            for (auto& p : prime_factors[i]) {
-                global_freq[p.first] += p.second;
-            }
+      int n = nums.size(), m = *std::max_element(nums.begin(), nums.end());
+      std::fill(l, l + m + 1, 0);
+      std::fill(r, r + m + 1, 0);
+      for (int i = 0; i < n; ++i) {
+        for (int x = nums[i]; x > 1; ) {
+          for (int d = a[x]; a[x] == d; x /= d) {
+            ++r[d];
+          }
         }
-
-        unordered_map<int, int> left_freq;
-        unordered_map<int, int> active_factors;  // Tracks factors that are still active in the right part
-
-        for (int i = 0; i < nums.size() - 1; i++) {  // No need to check the last element
-            for (auto& p : prime_factors[i]) {
-                left_freq[p.first] += p.second;
-                global_freq[p.first] -= p.second;
-                if (global_freq[p.first] == 0) {
-                    global_freq.erase(p.first);
-                } else {
-                    active_factors[p.first] = 1;  // Mark the factor as active on the right side
-                }
+      }
+      int cnt = 0;
+      for (int i = 0; i < n - 1; ++i) {
+        for (int x = nums[i]; x > 1; ) {
+          for (int d = a[x]; a[x] == d; x /= d) {
+            ++l[d];
+            --r[d];
+            if (l[d] == 1 && r[d] > 0) {
+              ++cnt;
+            } else if (l[d] > 1 && r[d] == 0) {
+              --cnt;
             }
-
-            // Check if any prime factor is still active in the right part
-            bool valid_split = true;
-            for (auto& factor : active_factors) {
-                if (global_freq.count(factor.first)) {
-                    valid_split = false;
-                    break;
-                }
-            }
-
-            if (valid_split) {
-                return i;
-            }
+          }
         }
-
-        return -1;
+        if (cnt == 0) {
+          return i;
+        }
+      }
+      return -1;
     }
 };
