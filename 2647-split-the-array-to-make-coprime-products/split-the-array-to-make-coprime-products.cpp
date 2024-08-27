a1af00756bf82240f0f49f4632e831fb98b4ef14
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-08-27 12:49:19 +0530

    Time: 1286 ms (21.28%) | Memory: 211 MB (23.83%) - LeetSync

--- 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
+++ 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
@@ -1,80 +1,78 @@
-constexpr int MAX = 1000100;
-
-int spf[MAX], primes[MAX], leftCnt[MAX], rightCnt[MAX];
-
-// Function to initialize the smallest prime factor for each number up to MAX
-void initPrimeFactors() {
-    int primeIdx = 0;
-    for (int i = 2; i < MAX; ++i) {
-        if (!spf[i]) {
-            spf[i] = i;
-            primes[primeIdx++] = i;
-        }
-        for (int j = 0; i * primes[j] < MAX; ++j) {
-            spf[i * primes[j]] = primes[j];
-            if (i % primes[j] == 0) {
-                break;
-            }
-        }
-    }
-}
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
 
 class Solution {
 public:
-    int findValidSplit(std::vector<int>& nums) {
-        // Initialize prime factors only once
-        static bool init = []() {
-            initPrimeFactors();
-            return true;
-        }();
-
-        int n = nums.size();
-        int maxElem = *std::max_element(nums.begin(), nums.end());
-
-        // Reset left and right counts for prime factors
-        std::fill(leftCnt, leftCnt + maxElem + 1, 0);
-        std::fill(rightCnt, rightCnt + maxElem + 1, 0);
-
-        // Populate the rightCnt array with prime factor counts from the entire array
-        for (int i = 0; i < n; ++i) {
-            int val = nums[i];
-            while (val > 1) {
-                int prime = spf[val];
-                while (val % prime == 0) {
-                    val /= prime;
-                    ++rightCnt[prime];
+    // Function to compute smallest prime factors (SPF) using the Sieve of Eratosthenes
+    std::vector<int> sieve(int max_num) {
+        std::vector<int> spf(max_num + 1);
+        for (int i = 2; i <= max_num; ++i) spf[i] = i;
+        for (int i = 2; i * i <= max_num; ++i) {
+            if (spf[i] == i) {  // i is prime
+                for (int j = i * i; j <= max_num; j += i) {
+                    if (spf[j] == j) spf[j] = i;
                 }
             }
         }
+        return spf;
+    }
 
-        int activeCnt = 0;
+    // Function to compute prime factor frequencies
+    void primeFreq(int x, std::unordered_map<int, int> &freq, const std::vector<int>& spf) {
+        while (x > 1) {
+            int prime = spf[x];
+            freq[prime]++;
+            x /= prime;
+        }
+    }
 
-        // Iterate through the array to find a valid split
-        for (int i = 0; i < n - 1; ++i) {  // No need to check the last element
-            int val = nums[i];
-            while (val > 1) {
-                int prime = spf[val];
-                while (val % prime == 0) {
-                    val /= prime;
-                    ++leftCnt[prime];
-                    --rightCnt[prime];
+    int findValidSplit(std::vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
 
-                    // Track the number of active prime factors on both sides
-                    if (leftCnt[prime] == 1 && rightCnt[prime] > 0) {
-                        ++activeCnt;
-                    } else if (leftCnt[prime] > 1 && rightCnt[prime] == 0) {
-                        --activeCnt;
-                    }
+        // Determine maximum number in the array for sieve size
+        int max_num = *std::max_element(nums.begin(), nums.end());
+        std::vector<int> spf = sieve(max_num);
+
+        // Global frequency of prime factors
+        std::unordered_map<int, int> global_freq;
+
+        // Calculate overall frequency of prime factors for the entire array
+        for (int num : nums) {
+            primeFreq(num, global_freq, spf);
+        }
+
+        std::unordered_map<int, int> left_freq;
+
+        // Iterate to find a valid split point
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            std::unordered_map<int, int> temp_freq;
+            primeFreq(nums[i], temp_freq, spf);
+
+            // Update left_freq and global_freq
+            for (const auto& [prime, count] : temp_freq) {
+                left_freq[prime] += count;
+                global_freq[prime] -= count;
+
+                if (global_freq[prime] == 0) {
+                    global_freq.erase(prime);
                 }
             }
 
-            // If there are no common prime factors, return the split index
-            if (activeCnt == 0) {
+            // Check for valid split
+            bool valid_split = true;
+            for (const auto& [prime, count] : left_freq) {
+                if (global_freq.count(prime)) {
+                    valid_split = false;
+                    break;
+                }
+            }
+
+            if (valid_split) {
                 return i;
             }
         }
 
-        // No valid split found
         return -1;
     }
 };
