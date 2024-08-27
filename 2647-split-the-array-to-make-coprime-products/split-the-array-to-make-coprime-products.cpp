ca0ce545d711716e8ace86db8fa94891273613cc
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-08-27 12:36:09 +0530

    Time: 288 ms (73.62%) | Memory: 55.5 MB (33.62%) - LeetSync

--- 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
+++ 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
@@ -1,57 +1,50 @@
 class Solution {
 public:
-    vector<int> sieve(int max_num) {
-        vector<int> spf(max_num + 1);  // Smallest prime factor for every number
-        for (int i = 2; i <= max_num; i++) spf[i] = i;
-        for (int i = 2; i * i <= max_num; i++) {
-            if (spf[i] == i) {  // i is prime
-                for (int j = i * i; j <= max_num; j += i) {
-                    if (spf[j] == j) spf[j] = i;
-                }
+    void primeFreq(int x, unordered_map<int, int> &mp) {
+        for (int i = 2; i * i <= x; i++) {
+            while (x % i == 0) {
+                mp[i] += 1;
+                x /= i;
             }
         }
-        return spf;
-    }
-
-    void primeFreq(int x, unordered_map<int, int> &mp, const vector<int>& spf) {
-        while (x != 1) {
-            mp[spf[x]] += 1;
-            x /= spf[x];
+        if (x > 1) {  // If x is a prime number
+            mp[x] += 1;
         }
     }
 
     int findValidSplit(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
-        int max_num = *max_element(nums.begin(), nums.end());
-        vector<int> spf = sieve(max_num);
 
+        // Global frequency of prime factors
         unordered_map<int, int> global_freq;
+        vector<unordered_map<int, int>> prime_factors(nums.size());
 
-        // Calculate overall frequency of prime factors
-        for (int num : nums) {
-            primeFreq(num, global_freq, spf);
+        // Calculate prime factor frequencies for each element and overall frequency
+        for (int i = 0; i < nums.size(); i++) {
+            primeFreq(nums[i], prime_factors[i]);
+            for (auto& p : prime_factors[i]) {
+                global_freq[p.first] += p.second;
+            }
         }
 
         unordered_map<int, int> left_freq;
+        unordered_map<int, int> active_factors;  // Tracks factors that are still active in the right part
 
         for (int i = 0; i < nums.size() - 1; i++) {  // No need to check the last element
-            unordered_map<int, int> temp_freq;
-            primeFreq(nums[i], temp_freq, spf);
-
-            // Move prime factors from right to left
-            for (auto& p : temp_freq) {
+            for (auto& p : prime_factors[i]) {
                 left_freq[p.first] += p.second;
                 global_freq[p.first] -= p.second;
-
                 if (global_freq[p.first] == 0) {
                     global_freq.erase(p.first);
+                } else {
+                    active_factors[p.first] = 1;  // Mark the factor as active on the right side
                 }
             }
 
-            // Check if there's no intersection between left and right parts
+            // Check if any prime factor is still active in the right part
             bool valid_split = true;
-            for (auto& p : left_freq) {
-                if (global_freq.count(p.first)) {
+            for (auto& factor : active_factors) {
+                if (global_freq.count(factor.first)) {
                     valid_split = false;
                     break;
                 }
