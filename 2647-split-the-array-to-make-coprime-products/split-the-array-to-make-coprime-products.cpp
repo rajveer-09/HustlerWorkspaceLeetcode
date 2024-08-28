3182b53c003be17fab67a60f9fe904ffa128ea1d
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-08-28 23:05:51 +0530

    Time: 260 ms (78.88%) | Memory: 55.4 MB (33.62%) - LeetSync

--- 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
+++ 2647-split-the-array-to-make-coprime-products/split-the-array-to-make-coprime-products.cpp
@@ -1,74 +1,53 @@
-#include <vector>
-#include <unordered_map>
-#include <algorithm>
-
 class Solution {
 public:
-    // Function to compute smallest prime factors (SPF) using the Sieve of Eratosthenes
-    std::vector<int> sieve(int max_num) {
-        std::vector<int> spf(max_num + 1);
-        for (int i = 2; i <= max_num; ++i) spf[i] = i;
-        for (int i = 2; i * i <= max_num; ++i) {
-            if (spf[i] == i) {  // i is prime
-                for (int j = i * i; j <= max_num; j += i) {
-                    if (spf[j] == j) spf[j] = i;
-                }
+    void factorFreq(int x, unordered_map<int, int> &mp) {
+        for (int i = 2; i * i <= x; i++) {
+            while (x % i == 0) {
+                mp[i]++;
+                x /= i;
             }
         }
-        return spf;
-    }
-
-    // Function to compute prime factor frequencies
-    void primeFreq(int x, std::unordered_map<int, int> &freq, const std::vector<int>& spf) {
-        while (x > 1) {
-            int prime = spf[x];
-            freq[prime]++;
-            x /= prime;
+        if (x > 1) {
+            mp[x]++;
         }
     }
 
-    int findValidSplit(std::vector<int>& nums) {
+    int findValidSplit(vector<int>& nums) {
         ios_base::sync_with_stdio(false);
 
-        // Determine maximum number in the array for sieve size
-        int max_num = *std::max_element(nums.begin(), nums.end());
-        std::vector<int> spf = sieve(max_num);
+        unordered_map<int, int> global;
+        vector<unordered_map<int, int>> factors(nums.size());
 
-        // Global frequency of prime factors
-        std::unordered_map<int, int> global_freq;
-
-        // Calculate overall frequency of prime factors for the entire array
-        for (int num : nums) {
-            primeFreq(num, global_freq, spf);
+        for (int i = 0; i < nums.size(); i++) {
+            factorFreq(nums[i], factors[i]);
+            for (auto& p : factors[i]) {
+                global[p.first] += p.second;
+            }
         }
 
-        std::unordered_map<int, int> left_freq;
+        unordered_map<int, int> left;
+        unordered_map<int, int> active;
 
-        // Iterate to find a valid split point
-        for (int i = 0; i < nums.size() - 1; ++i) {
-            std::unordered_map<int, int> temp_freq;
-            primeFreq(nums[i], temp_freq, spf);
-
-            // Update left_freq and global_freq
-            for (const auto& [prime, count] : temp_freq) {
-                left_freq[prime] += count;
-                global_freq[prime] -= count;
-
-                if (global_freq[prime] == 0) {
-                    global_freq.erase(prime);
+        for (int i = 0; i < nums.size() - 1; i++) {
+            for (auto& p : factors[i]) {
+                left[p.first] += p.second;
+                global[p.first] -= p.second;
+                if (global[p.first] == 0) {
+                    global.erase(p.first);
+                } else {
+                    active[p.first] = 1;
                 }
             }
 
-            // Check for valid split
-            bool valid_split = true;
-            for (const auto& [prime, count] : left_freq) {
-                if (global_freq.count(prime)) {
-                    valid_split = false;
+            bool valid = true;
+            for (auto& a : active) {
+                if (global.count(a.first)) {
+                    valid = false;
                     break;
                 }
             }
 
-            if (valid_split) {
+            if (valid) {
                 return i;
             }
         }
