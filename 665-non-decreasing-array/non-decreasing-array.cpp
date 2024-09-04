c1105a3a971fd2af74f0e9e0052e1a83a8e0dffd
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-04 12:54:49 +0530

    Time: 15 ms (80.82%) | Memory: 29.9 MB (43.34%) - LeetSync

---
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) {
+                cnt++;
+                if (cnt > 1)
+                    return false;
+
+                if (i > 1 && nums[i] < nums[i - 2]) {
+                    nums[i] = nums[i - 1];
+                }
+            }
+        }
+
+        return true;
+    }
+};
