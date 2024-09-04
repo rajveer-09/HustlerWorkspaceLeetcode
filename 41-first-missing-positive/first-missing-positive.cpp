26f47a0ed64e426b20e753883a3e338dfc670771
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-04 15:35:47 +0530

    Time: 72 ms (24.49%) | Memory: 55 MB (27.41%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-        ios_base::sync_with_stdio(false);
-        int n = nums.size();
-        vector<int> mp(n + 1, 0);
+    int firstMissingPositive(vector<int>& arr) {
+        int n = arr.size();
 
-        for (int num : nums) {
-            if (num > 0 && num <= n) {
-                mp[num] = 1;
+        sort(arr.begin(), arr.end());
+
+        int target = 1;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] <= 0) {
+                continue;
+            }
+
+            if (arr[i] == target) {
+                target++;
+            }
+
+            else if (arr[i] > target) {
+                return target;
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (mp[i] == 0) {
-                return i;
-            }
-        }
-
-        return n + 1;
+        return target;
     }
 };
