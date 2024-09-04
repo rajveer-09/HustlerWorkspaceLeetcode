405fcdb820b30e11ba94f622e155ae1d823a4c3d
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-04 14:50:00 +0530

    Time: 22 ms (32.70%) | Memory: 29.8 MB (43.34%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.cpp
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int count = 0;
+        bool changed = false;
 
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                count++;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] <= nums[i + 1]) continue;
 
-                if (count > 1) {
-                    return false;
-                }
-                if (i > 0 && nums[i - 1] > nums[i + 1]) {
-                    nums[i + 1] = nums[i];
-                } 
-                else {
-                    nums[i] = nums[i + 1];
-                }
+            if (changed) return false;
+
+            if (i == 0 || nums[i + 1] >= nums[i - 1]) {
+                nums[i] = nums[i + 1];
+            } else {
+                nums[i + 1] = nums[i];
             }
+            changed = true;
         }
 
         return true;
