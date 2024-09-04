70f90dd78a0a1033c2f9edac972edf2f2dda5dfd
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-04 13:01:01 +0530

    Time: 14 ms (86.48%) | Memory: 29.8 MB (43.34%) - LeetSync

--- 665-non-decreasing-array/non-decreasing-array.cpp
+++ 665-non-decreasing-array/non-decreasing-array.cpp
@@ -1,27 +1,20 @@
-#pragma GCC target("tune=native")
-#pragma GCC optimize("Ofast")
-//#pragma GCC optimize("Os")
-// Code here is optimized for size
-
-static const auto fastIO = []() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return 0;
-}();
 class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
-        int cnt = 0;
+        int count = 0;
 
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < nums[i - 1]) {
-                cnt++;
-                if (cnt > 1)
+        for (int i = 0; i < nums.size() - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+
+                if (count > 1) {
                     return false;
-
-                if (i > 1 && nums[i] < nums[i - 2]) {
-                    nums[i] = nums[i - 1];
+                }
+                if (i > 0 && nums[i - 1] > nums[i + 1]) {
+                    nums[i + 1] = nums[i];
+                } 
+                else {
+                    nums[i] = nums[i + 1];
                 }
             }
         }
