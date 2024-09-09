b4272e615cfd698785e7b6105529e1a7a7c6430b
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-09 19:38:11 +0530

    Time: 22 ms (70.68%) | Memory: 95.2 MB (40.17%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        ios_base::sync_with_stdio(false);
         int m= nums1.size();
         int n= nums2.size();
         vector<int> merged(m + n);
