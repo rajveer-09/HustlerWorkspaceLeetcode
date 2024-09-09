6d426ebb8e9e8a366c82d3f62ccd5ffb93427a30
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-09 19:36:44 +0530

    Time: 24 ms (60.98%) | Memory: 94.9 MB (47.97%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,54 +2,63 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
         ios_base::sync_with_stdio(false);
-        vector<int> arr3;
         int n1 = a.size(), n2 = b.size();
+        int n = n1 + n2; // total size
+        // required indices:
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+        int cnt = 0;
+        int ind1el = -1, ind2el = -1;
+
+        // apply the merge step:
         int i = 0, j = 0;
-
-        int n = n1 + n2;
-
         while (i < n1 && j < n2) {
             if (a[i] < b[j]) {
-                arr3.push_back(a[i++]);
+                if (cnt == ind1)
+                    ind1el = a[i];
+                if (cnt == ind2)
+                    ind2el = a[i];
+                cnt++;
+                i++;
             } else {
-                arr3.push_back(b[j++]);
+                if (cnt == ind1)
+                    ind1el = b[j];
+                if (cnt == ind2)
+                    ind2el = b[j];
+                cnt++;
+                j++;
             }
-
-            if (arr3.size() > (n) / 2 + 1) {
-                if (n % 2 == 1) {
-                    return arr3[n / 2];
-                } else {
-                    return (double)(arr3[n / 2] + arr3[n / 2 - 1]) / 2.0;
-                }
-            }
+            if (ind1el != -1 && ind2el != -1)
+                break;
         }
 
+        // copy the left-out elements:
         while (i < n1) {
-            arr3.push_back(a[i++]);
-            if (arr3.size() > (n) / 2 + 1) {
-                if (n % 2 == 1) {
-                    return arr3[n / 2];
-                } else {
-                    return (double)(arr3[n / 2] + arr3[n / 2 - 1]) / 2.0;
-                }
-            }
+            if (cnt == ind1)
+                ind1el = a[i];
+            if (cnt == ind2)
+                ind2el = a[i];
+            cnt++;
+            i++;
+            if (ind1el != -1 && ind2el != -1)
+                break;
         }
-
         while (j < n2) {
-            arr3.push_back(b[j++]);
-            if (arr3.size() > (n) / 2 + 1) {
-                if (n % 2 == 1) {
-                    return arr3[n / 2];
-                } else {
-                    return (double)(arr3[n / 2] + arr3[n / 2 - 1]) / 2.0;
-                }
-            }
+            if (cnt == ind1)
+                ind1el = b[j];
+            if (cnt == ind2)
+                ind2el = b[j];
+            cnt++;
+            j++;
+            if (ind1el != -1 && ind2el != -1)
+                break;
         }
 
+        // Find the median:
         if (n % 2 == 1) {
-            return arr3[n / 2];
-        } else {
-            return (double)(arr3[n / 2] + arr3[n / 2 - 1]) / 2.0;
+            return (double)ind2el;
         }
+
+        return (double)((double)(ind1el + ind2el)) / 2.0;
     }
 };
