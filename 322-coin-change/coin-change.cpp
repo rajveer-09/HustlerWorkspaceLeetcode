4bbef220676530d5d01ae0d5197fa2130f81ea4c
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-12 03:23:38 +0530

    Time: 32 ms (62.36%) | Memory: 17.6 MB (56.60%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX);
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (i - coins[j] >= 0) {
-                    int part = dp[i - coins[j]];
-                    if (part != INT_MAX) {
-                        dp[i] = min(dp[i], 1 + part);
-                    }
-                }
+    int fun(vector<int>& coins, int amount, vector<int>& dp) {
+        if (amount == 0)
+            return 0;
+
+        if (amount < 0)
+            return INT_MAX; 
+
+        if (dp[amount] != -1)
+            return dp[amount];
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < coins.size(); i++) {
+            int part = fun(coins, amount - coins[i], dp);
+            if (part != INT_MAX) {
+                ans = min(ans, 1 + part);
             }
         }
+        
+        dp[amount] = ans;
+        return dp[amount];
+    }
 
-        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, -1);
+
+        int ans = fun(coins, amount, dp);
+
+        return (ans == INT_MAX) ? -1 : ans;
     }
 };
