da6968651f08c992d6ecc14af103ac70fb7e9ad2
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-13 01:07:59 +0530

    Time: 11 ms (70.82%) | Memory: 14 MB (10.53%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,16 +1,41 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& A) {
-        const int n = A.size();
+    int memo(vector<vector<int>>& matrix, int idx, int col, vector<vector<int>>& dp) {
+        int n = matrix.size();
 
-        for (int i = 1; i < n; ++i)
-            for (int j = 0; j < n; ++j) {
-                int mn = INT_MAX;
-                for (int k = max(0, j - 1); k < min(n, j + 2); ++k)
-                    mn = min(mn, A[i - 1][k]);
-                A[i][j] += mn;
-            }
+        if (idx >= n ) return 0;
+        
+        if (col < 0 || col >= n) return INT_MAX;
 
-        return ranges::min(A[n - 1]);
+        if (idx == n - 1 ) return matrix[idx][col];
+
+        if (dp[idx][col] != INT_MAX) return dp[idx][col];
+
+        int btl = INT_MAX, btm = INT_MAX, btr = INT_MAX;
+
+        if (col - 1 >= 0) {
+            btl = matrix[idx][col] + memo(matrix, idx + 1, col - 1, dp);
+        }
+        
+        btm = matrix[idx][col] + memo(matrix, idx + 1, col, dp);
+
+        if (col + 1 < n) {
+            btr = matrix[idx][col] + memo(matrix, idx + 1, col + 1, dp);
+        }
+
+        return dp[idx][col] = min(btm, min(btr, btl));
+    }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        ios_base::sync_with_stdio(false);
+        int n = matrix.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+
+        int ans = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            ans = min(ans, memo(matrix, 0, i, dp));
+        }
+        return ans;
     }
 };
