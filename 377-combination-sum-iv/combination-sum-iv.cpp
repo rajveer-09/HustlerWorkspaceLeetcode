4d9827530a191f2f2e9305283c9bebb324c7c198
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-12 11:00:47 +0530

    Time: 3 ms (45.88%) | Memory: 8.2 MB (59.79%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<unsigned long long> dp(target + 1, 0);
+        vector<int> dp(target + 1, 0);
         dp[0] = 1;  // Base case: one way to make zero sum (using no elements)
 
         for (int i = 1; i <= target; i++) {
-            for (int j = 0; j < nums.size(); j++) {
-                if (nums[j] <= i) {
-                    dp[i] += dp[i - nums[j]];
+            for (int num : nums) {
+                if (num <= i) {
+                    // Check for potential overflow before updating dp[i]
+                    if (dp[i] > INT_MAX - dp[i - num]) {
+                        // If overflow is detected, set dp[i] to a value that signifies an invalid state
+                        dp[i] = INT_MAX;
+                    } 
+                    else {
+                        dp[i] += dp[i - num];
+                    }
                 }
             }
         }
