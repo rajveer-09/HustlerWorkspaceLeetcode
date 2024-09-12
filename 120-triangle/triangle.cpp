1070e2c3335d602bb008615b827e156afa607b7b
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-13 02:13:18 +0530

    Time: 2 ms (88.02%) | Memory: 11.6 MB (59.32%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int func(int i, int j, vector<vector<int> > &triangle, vector<vector<int>> &dp) {
+        int n = triangle.size();
+
+        if (i == n - 1) return triangle[i][j]; 
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int down = triangle[i][j] + func(i + 1, j, triangle, dp);
+
+        int diagonal = triangle[i][j] + func(i + 1, j + 1, triangle, dp);
+
+        return dp[i][j] = min(down, diagonal);
+   }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        ios_base::sync_with_stdio(false);
+        int n = triangle.size();
+        vector<vector<int> > dp(n, vector<int>(n, -1));
+        return func(0, 0, triangle, dp);
+    }
+};
