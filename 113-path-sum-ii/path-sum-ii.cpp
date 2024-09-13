2e96216277155dd657e4ef033c926b98649eef87
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-14 04:10:37 +0530

    Time: 12 ms (43.55%) | Memory: 36.8 MB (32.83%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution{
+public:
+    void solve(TreeNode *root, int targetSum, vector<vector<int>> &path,vector<int> ans, int sum){
+        if (root == NULL)
+        {
+            return;
+        }
+        sum += root->val;
+        ans.push_back(root->val);
+        if (!root->left && !root->right)
+        {
+            if (sum == targetSum)
+            {
+                path.push_back(ans);
+            }
+            return;
+        }
+        solve(root->left, targetSum, path, ans, sum);
+        solve(root->right, targetSum, path, ans, sum);
+    }
+    vector<vector<int>> pathSum(TreeNode *root, int targetSum){
+        int sum = 0;
+        vector<int> ans;
+        vector<vector<int>> path;
+        solve(root, targetSum, path, ans, sum);
+        return path;
+    }
+};
