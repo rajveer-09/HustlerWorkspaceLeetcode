7fe070c545d71d284ae9fa08be96f617d04fb521
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-09-15 06:15:38 +0530

    Time: 750 ms (66.48%) | Memory: 281 MB (80.78%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        unordered_map<int, vector<int>> graph;
-        for (const auto& edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-            graph[u].push_back(v);
-            graph[v].push_back(u);
+        vector<vector<int>> adj(n);
+
+        for(int i = 0; i < edges.size(); i++){
+            int x = edges[i][0];
+            int y = edges[i][1];
+            adj[x].push_back(y);
+            adj[y].push_back(x);
         }
-        
-        unordered_set<int> visited;
-        return dfs(source, destination, graph, visited);
+
+        return bfs(source, adj, destination);
     }
-    
-    bool dfs(int node, int destination, unordered_map<int, vector<int>>& graph, unordered_set<int>& visited) {
-        if (node == destination) {
-            return true;
-        }
-        visited.insert(node);
-        for (int neighbor : graph[node]) {
-            if (visited.find(neighbor) == visited.end()) {
-                if (dfs(neighbor, destination, graph, visited)) {
-                    return true;
+private :
+    bool bfs(int start, const vector<vector<int>>& adj, int des) {
+        vector<bool> visited(adj.size(), false);
+        queue<int> q;
+
+        q.push(start);
+        visited[start] = true;
+
+        while (!q.empty()) {
+            int node = q.front();
+            if(node == des) return true;
+            q.pop();
+            cout << node << " ";
+
+            for (int neighbor : adj[node]) {
+                if (!visited[neighbor]) {
+                    q.push(neighbor);
+                    visited[neighbor] = true;
                 }
             }
         }
+
         return false;
     }
 };
