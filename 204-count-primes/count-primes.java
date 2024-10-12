7caf9660ee2e21883583011219868609ba5f544f
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-10-12 16:00:26 +0530

    Time: 101 ms (53.44%) | Memory: 48.7 MB (91.59%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -4,13 +4,15 @@ class Solution {
 
         boolean[] isPrime = new boolean[n];
         for (int i = 2; i < n; i++) {
-            isPrime[i] = true; // Assume all numbers are prime initially
+            // Assume all numbers are prime initially
+            isPrime[i] = true;
         }
 
         for (int i = 2; i * i < n; i++) {
             if (isPrime[i]) {
                 for (int j = i * i; j < n; j += i) {
-                    isPrime[j] = false; // Mark multiples of i as not prime
+                    // Mark multiples of i as not prime
+                    isPrime[j] = false;
                 }
             }
         }
