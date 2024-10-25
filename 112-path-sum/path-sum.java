40b4cae9f706814a16cc433a289faa2cd23aee79
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-10-25 19:36:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (7.51%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -3,15 +3,19 @@ class Solution {
         if (node == null) {
             return false;
         }
-
        
-        if (node.left == null && node.right == null && sum == node.val){
+        if (node.left == null && node.right == null 
+            && sum == node.val){
+
             return true;
         }
 
         // Recur down the left and right subtrees
         int remSum = sum - node.val;
+
+        boolean leftAns = hasPathSum(node.left, remSum);
+        boolean rightAns = hasPathSum(node.right, remSum);
         
-        return hasPathSum(node.left, remSum) || hasPathSum(node.right, remSum);
+        return leftAns || rightAns;
     }
 }
