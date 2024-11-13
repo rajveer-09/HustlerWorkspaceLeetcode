e73d1ba25671903b3a610600737dc192d990c3bb
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-11-13 16:52:05 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (59.46%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
@@ -1,48 +1,17 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-
- int sum = 0;
 class Solution {
 public:
-    void solve(TreeNode* root , int& sum){
-        if(root==NULL){
-            return;
-        }
-        if(root->val % 2 == 0){
-            if(root->left){
-                if(root->left->left){
-                sum+=root->left->left->val;
-                }
-                if(root->left->right){
-                    sum+=root->left->right->val;
-                }
-            }
-            
-            if(root->right){
-                if(root->right->right){
-                    sum+=root->right->right->val;
-                }
-                if(root->right->left){
-                    sum+=root->right->left->val;
-                }
-            }
-        }
-       
-        solve(root->left , sum);
-        solve(root->right , sum);
-     }
+    void solve(TreeNode* root, int a, int b, int& sum) {
+        if(!root) return;
+
+        if(a % 2 == 0) sum += root->val;
+
+        solve(root->left, b, root->val, sum);
+        solve(root->right, b, root->val, sum);
+    }
     int sumEvenGrandparent(TreeNode* root) {
         int sum = 0;
-        solve(root,sum);
+        solve(root, -1, -1, sum);
+
         return sum;
     }
 };
