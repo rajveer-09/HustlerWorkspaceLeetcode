1527d3f1657b919c09f1629cfcb3bdb1d8ef8a0c
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-11-19 11:14:01 +0530

    Time: 3 ms (89.30%) | Memory: 11.5 MB (55.85%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    bool isVowel(char& ch){
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') return true;
-
-        return false;
-    }
-
-    int count(const unordered_map<char, int>& mp){
-        int cnt = 0;
-        for(auto& it : mp){
-            cnt += it.second;
-        }
-
-        return cnt;
+    bool isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
 
     int maxVowels(string s, int k) {
-        unordered_map<char, int> mp;
+        int left = 0;
+        int maxV = 0;
+        int currV = 0;
 
-        int st = 0;
-        int maxi = 0;
-
-        for(int ed = 0; ed < s.size(); ed++){
-            if(isVowel(s[ed])){
-                mp[s[ed]]++;
+        for (int right = 0; right < s.size(); right++) {
+            if (isVowel(s[right])) {
+                currV++;
             }
 
-            if(ed - st + 1 == k){
-                maxi = max(maxi, count(mp));
-                if(isVowel(s[st])){
-                    mp[s[st]]--;
+            if (right - left + 1 > k) {
+                if (isVowel(s[left])) {
+                    currV--;
                 }
-                st++;
+                left++;
+            }
+
+            if (right - left + 1 == k) {
+                maxV = max(maxV, currV);
             }
         }
 
-        return maxi;
+        return maxV;
     }
 };
