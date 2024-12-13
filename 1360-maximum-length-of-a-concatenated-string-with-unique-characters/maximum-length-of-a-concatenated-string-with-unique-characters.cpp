0bc8e46f466606ec7ce7e1fb18634fa1074bd693
Rajveer Sharma <rajveersharma1107@gmail.com>
2024-12-14 00:45:12 +0530

    Time: 35 ms (67.09%) | Memory: 29.6 MB (63.65%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
-    int maxLength(vector<string>& arr) {
-        vector<int> dp = {0};
-        int res = 0;
-        
-        for (const string& s : arr) {
-            int a = 0, dup = 0;
-            for (char c : s) {
-                dup |= a & (1 << (c - 'a'));
-                a |= 1 << (c - 'a');
-            }
-            
-            if (dup > 0)
-                continue;
-            
-            for (int i = dp.size() - 1; i >= 0; i--) {
-                if ((dp[i] & a) > 0)
-                    continue;
-                dp.push_back(dp[i] | a);
-                res = max(res, __builtin_popcount(dp[i] | a));
-            }
+    bool hasDupli(string& s1, string& s2) {
+        int arr[26] = {0};
+        for (char& ch : s1) {
+            if (arr[ch - 'a'] != 0) return true;
+            arr[ch - 'a']++;
         }
-        
-        return res;
+
+        for (char& ch : s2) {
+            if (arr[ch - 'a'] != 0) return true;
+        }
+
+        return false;
+    }
+
+    unordered_map<string, int> dp;
+
+    int memo(vector<string>& arr, int n, int idx, string ans) {
+        if (idx >= n) return ans.size();
+
+        if (dp.find(ans) != dp.end()) return dp[ans];
+
+        int inc = 0;
+        int exc = 0;
+
+        if (hasDupli(arr[idx], ans)) {
+            exc = memo(arr, n, idx + 1, ans);
+        } else {
+            inc = memo(arr, n, idx + 1, ans + arr[idx]);
+            exc = memo(arr, n, idx + 1, ans);
+        }
+
+        return dp[ans] = max(inc, exc);
+    }
+
+    int maxLength(vector<string>& arr) {
+        int n = arr.size();
+        if (n == 0) return 0;
+
+        dp.clear();
+
+        return memo(arr, n, 0, "");
     }
 };
