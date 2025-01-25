59c7e32ab46e30eb8d68e88d26640c3697469138
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-01-26 02:19:38 +0530

    Time: 331 ms (17.65%) | Memory: 201 MB (16.81%) - LeetSync

--- 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-         unordered_map<int,int>lost_map;
-        for(int i=0;i<matches.size();i++)
-        {
-            int lose=matches[i][1];
-        lost_map[lose]++;
+        unordered_map<int, int> losses;
+        set<int> players;
+
+        for (const auto& match : matches) {
+            losses[match[1]]++;
+            players.insert(match[0]);
+            players.insert(match[1]);
         }
-        vector<int>notLost;
-        vector<int>onceLost;
-        for(int i=0;i<matches.size();i++)
-        {
-           int winner=matches[i][0];
-           int looser=matches[i][1];
-           if(lost_map.find(winner)==lost_map.end()) 
-           {
-               notLost.push_back(winner);
-               lost_map[winner]=2;
 
-           }
-           if(lost_map[looser]==1)
-           onceLost.push_back(looser);
+        vector<int> ans1, ans2;
 
+        for (int player : players) {
+            if (losses[player] == 0) {
+                ans1.push_back(player);
+            } 
+            else if (losses[player] == 1) {
+                ans2.push_back(player);
+            }
         }
-        sort(notLost.begin(),notLost.end());    
-        sort(onceLost.begin(),onceLost.end());
 
-        return {notLost,onceLost};
+        sort(ans1.begin(), ans1.end());
+        sort(ans2.begin(), ans2.end());
+
+        return {ans1, ans2};
     }
 };
