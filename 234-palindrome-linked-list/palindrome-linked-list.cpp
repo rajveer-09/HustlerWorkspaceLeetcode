2e02d3c6666c68120dd76cc3a8732acd9a81a4ee
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-02-22 13:39:55 +0530

    Time: 4 ms (54.76%) | Memory: 131.9 MB (19.15%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,31 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
+        vector<int> ans;
 
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* prev = NULL;
+        ListNode* temp = head;
 
-        // Find the middle of the linked list and reverse the first half
-        while (fast && fast->next) {
-            fast = fast->next->next;
-            ListNode* temp = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = temp;
+        while(temp != NULL){
+            ans.push_back(temp->val);
+            temp = temp->next;
         }
 
-        // If the number of nodes is odd, move slow to the next node
-        if (fast)
-            slow = slow->next;
-
-        // Compare the reversed first half with the second half
-        while (prev && slow) {
-            if (prev->val != slow->val)
-                return false;
-            prev = prev->next;
-            slow = slow->next;
+        for(int i = 0; i < ans.size() / 2; i++){
+            if(ans[i] != ans[ans.size() - i - 1]) return false;
         }
 
         return true;
