eca9bcf9bb17cdf4750e2dbf40877243541ddd63
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-03-07 17:11:57 +0530

    Time: 58 ms (16.20%) | Memory: 56.3 MB (97.66%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -1,35 +1,32 @@
 class Solution {
     public long maximumSubarraySum(int[] nums, int k) {
+        long sum = 0;
+        long ans  =0 ;
         int st = 0;
-        long sum = 0, ans = 0;
-        Map<Integer, Integer> freq = new HashMap<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0) + 1);
+        HashMap <Integer,Integer> mpp = new HashMap<>();
+        for (int i = 0; i< nums.length; i++) {
+            mpp.put(nums[i] , mpp.getOrDefault(nums[i] , 0) +1);
             sum += nums[i];
-
-            // Ensure all elements are unique
-            while (freq.get(nums[i]) > 1) {
-                freq.put(nums[st], freq.get(nums[st]) - 1);
+            while(mpp.get(nums[i]) > 1) {
+                mpp.put(nums[st], mpp.get(nums[st]) -1);
                 sum -= nums[st];
-                if (freq.get(nums[st]) == 0) freq.remove(nums[st]);
+                if (mpp.get(nums[st]) == 0) {
+                    mpp.remove(nums[st]);
+                }
                 st++;
             }
+            if(i-st+1 == k ) {
+                if(mpp.size() == k) ans = Math.max(sum,ans);
+                
+                mpp.put(nums[st], mpp.get(nums[st]) -1);
+                sum -=nums[st];
 
-            // If window size is exactly k, check for max sum
-            if (i - st + 1 == k) {
-                if (freq.size() == k) {
-                    ans = Math.max(ans, sum);
+                if (mpp.get(nums[st]) == 0) {
+                    mpp.remove(nums[st]);
                 }
-
-                // Remove leftmost element to slide the window
-                freq.put(nums[st], freq.get(nums[st]) - 1);
-                sum -= nums[st];
-                if (freq.get(nums[st]) == 0) freq.remove(nums[st]);
                 st++;
             }
         }
-
         return ans;
     }
 }
