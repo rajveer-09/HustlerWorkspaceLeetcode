3eee33399ca20c51696435269a070f1ac4f950af
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-03-18 09:43:57 +0530

    Time: 2 ms (37.79%) | Memory: 15.1 MB (39.48%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int memo(vector<vector<int>>& matrix, int idx, int col, vector<vector<int>>& dp) {
+    int memo(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>& dp){
         int n = matrix.size();
+        int m = matrix[0].size();
 
-        if (idx >= n ) return 0;
+        if(i < 0 || j < 0 || j >= m) return INT_MAX;
+
+        if(i == 0) return matrix[i][j];
+
+        if(dp[i][j] != INT_MAX) return dp[i][j];
+
+        long long d = matrix[i][j] * 1LL + memo(matrix, i - 1, j, dp);
+        long long rd = matrix[i][j] * 1LL + memo(matrix, i - 1, j - 1, dp);
+        long long ld = matrix[i][j] * 1LL + memo(matrix, i - 1, j + 1, dp);
+
+        return dp[i][j] = (int)min({d, rd, ld});
         
-        if (col < 0 || col >= n) return INT_MAX;
-
-        if (idx == n - 1 ) return matrix[idx][col];
-
-        if (dp[idx][col] != INT_MAX) return dp[idx][col];
-
-        int btl = INT_MAX, btm = INT_MAX, btr = INT_MAX;
-
-        if (col - 1 >= 0) {
-            btl = matrix[idx][col] + memo(matrix, idx + 1, col - 1, dp);
-        }
-        
-        btm = matrix[idx][col] + memo(matrix, idx + 1, col, dp);
-
-        if (col + 1 < n) {
-            btr = matrix[idx][col] + memo(matrix, idx + 1, col + 1, dp);
-        }
-
-        return dp[idx][col] = min(btm, min(btr, btl));
     }
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        ios_base::sync_with_stdio(false);
-        int n = matrix.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
 
         int ans = INT_MAX;
 
-        for (int i = 0; i < n; i++) {
-            ans = min(ans, memo(matrix, 0, i, dp));
+        for(int j = 0; j < m; j++){
+            ans = min(ans, memo(matrix, n - 1, j, dp)); 
+
         }
+
         return ans;
     }
 };
