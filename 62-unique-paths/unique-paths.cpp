586e066fbb240b2a03a741a2fc7418d13178d318
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-03-19 06:45:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (33.09%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -8,15 +8,24 @@ public:
         if (dp[x][y] != -1) return dp[x][y];
 
         int down = memo(m, n, x + 1, y, dp);
+        
         int right = memo(m, n, x, y + 1, dp);
 
         return dp[x][y] = down + right;
     }
 
     int uniquePaths(int m, int n) {
-        ios_base::sync_with_stdio(false);
         vector<vector<int>> dp(m, vector<int>(n, -1));
 
-        return memo(m, n, 0, 0, dp);
+        for(int j = 0; j < n; j++) dp[0][j] = 1;
+        for(int i = 0; i < m; i++) dp[i][0] = 1;
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 };
