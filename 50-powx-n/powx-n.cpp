5bbdb68f097c926a8260a723511e7ea3c13e40fb
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-03-31 19:22:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.85%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     double fun(double a, long long b) {
         if (b == 0) return 1;
+
         if (b < 0) {
             return 1.0 / fun(a, -b);
         }
         
         double half = fun(a, b / 2);
+
         if (b % 2 == 0) {
             return half * half;
-        } else {
+        } 
+        else {
             return half * half * a;
         }
     }
