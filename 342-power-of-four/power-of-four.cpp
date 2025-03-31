0bbcbafde91694522054aa846ff22790c474e97e
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-03-31 18:33:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (83.97%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n == 1) return true;
-        if(n < 4) return false;
-        return n % 4 == 0 && isPowerOfFour(n / 4);
+        // Base cases
+        if (n == 1) return true;
+        if (n <= 0 || n % 4 != 0) return false;
+
+        // Recursive step
+        return isPowerOfFour(n / 4);
     }
 };
