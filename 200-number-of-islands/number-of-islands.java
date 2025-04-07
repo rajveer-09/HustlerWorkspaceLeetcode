68e179d60275d111908ca4fb377ec33e5a02673a
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-04-07 19:33:05 +0530

    Time: 5 ms (34.08%) | Memory: 52.1 MB (22.03%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,41 +1,53 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
+    public void bfs(char[][] grid, int i, int j, boolean vis[][]){
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{i, j});
+
+        vis[i][j] = true;
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        while(!q.isEmpty()){
+            int[] node = q.poll();
+            int x = node[0];
+            int y = node[1];
+
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                if(nx >= 0 && nx < n  && ny >= 0 && ny < m){
+                    if(grid[nx][ny] == '1' && !vis[nx][ny]){
+                        q.add(new int[]{nx, ny});
+                        vis[nx][ny] = true;
+                    }
+                }
+            }
+            
         }
 
-        int numIslands = 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
+    }
+    public int numIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        boolean vis[][] = new boolean [n][m];
+        int cnt = 0;
+
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <  m; j++){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    bfs(grid, i, j, vis);
+                    cnt++;
                 }
             }
         }
 
-        return numIslands;
-    }
-
-    private void dfs(char[][] grid, int i, int j) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        // Boundary and base condition check
-        if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] == '0') {
-            return;
-        }
-
-        // Mark the cell as visited by setting it to '0'
-        grid[i][j] = '0';
-
-        // Perform DFS in all four directions
-        dfs(grid, i - 1, j); // Up
-        dfs(grid, i + 1, j); // Down
-        dfs(grid, i, j - 1); // Left
-        dfs(grid, i, j + 1); // Right
+        return cnt;
     }
 }
