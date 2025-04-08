adf9da68b511bf0b59488e6e2c4f7bf954b0192d
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-04-08 19:01:40 +0530

    Time: 8 ms (77.68%) | Memory: 58.4 MB (38.52%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void dfs(int[][] grid, int i, int j, boolean vis[][]){
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        if(i < 0 || i >= n || j < 0 || j >= m) return;
+        if(grid[i][j] == 0 || vis[i][j]) return;
+
+        vis[i][j] = true;
+        grid[i][j] = 0;
+
+        
+        dfs(grid, i - 1, j, vis);
+        dfs(grid, i + 1, j, vis);
+        dfs(grid, i, j - 1, vis);
+        dfs(grid, i, j + 1, vis);
+    }
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean vis[][] = new boolean[n][m]; 
+
+        for(int i  = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(i == 0 || j == 0 || i == n - 1 || j == m-1){
+                    if(grid[i][j] == 1 && !vis[i][j]){
+                        dfs(grid, i, j, vis);
+                    }
+                }
+            }
+        }
+
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1) cnt++;
+            }
+        }
+
+        return cnt;
+
+    }
+}
