3608a769a2fd58d851aa9f45de14101683ec3725
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-04-21 19:15:26 +0530

    Time: 205 ms (5.01%) | Memory: 90.8 MB (6.60%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -5,14 +5,13 @@ public:
         int ans = 0;
         int n = s.size();
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < n; ){
             unordered_set<char> st;
 
             while(i < n && !st.count(s[i])){
                 st.insert(s[i]);
                 i++;
             }
-            i--;
 
             ans++;
         }
