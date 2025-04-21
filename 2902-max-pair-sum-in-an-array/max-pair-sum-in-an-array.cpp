e49904b319399d2398a0e99f40e6c864c45731c7
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-04-21 16:47:42 +0530

    Time: 6 ms (69.81%) | Memory: 76.4 MB (29.71%) - LeetSync

---
+++ 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int lgd(int n) {
+        int ans = 0;
+        while(n) {
+            ans = max(ans, n % 10);
+            n /= 10;
+        }
+        return ans;
+    }
+
+    int maxSum(vector<int>& nums) {
+        map<int, priority_queue<int, vector<int>, greater<int>>> mpp;
+
+        for(int num : nums) {
+            int d = lgd(num);
+            mpp[d].push(num);
+            if(mpp[d].size() > 2) {
+                mpp[d].pop();
+            }
+        }
+
+        int maxi = -1;
+
+        for(auto& it : mpp) {
+            if(it.second.size() >= 2) {
+                int n1 = it.second.top(); it.second.pop();
+                int n2 = it.second.top();
+                maxi = max(maxi, n1 + n2);
+            }
+        }
+
+        return maxi;
+    }
+};
