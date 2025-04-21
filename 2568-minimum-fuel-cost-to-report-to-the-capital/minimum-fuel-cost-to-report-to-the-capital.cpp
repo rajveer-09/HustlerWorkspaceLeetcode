6a6d01aea44c5986dd8fdfbf7d12ecdcdcf5c8a8
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-04-21 19:07:28 +0530

    Time: 123 ms (98.91%) | Memory: 167.2 MB (98.36%) - LeetSync

--- 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,32 +1,30 @@
 class Solution {
 private:
-    vector<vector<int>> adj;
     long long res = 0;
+
+    int dfs(int node, int parent, int seats, vector<int> adj[]) {
+        int passengers = 0;
+        for (int child : adj[node]) {
+            if (child != parent) {
+                int p = dfs(child, node, seats, adj);
+                passengers += p;
+                res += ceil((double)p / seats);
+            }
+        }
+        return passengers + 1;
+    }
 
 public:
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
         int n = roads.size() + 1;
-        adj.resize(n);
+        vector<int> adj[n];
 
         for (auto& road : roads) {
             adj[road[0]].push_back(road[1]);
             adj[road[1]].push_back(road[0]);
         }
 
-        dfs(0, -1, seats);
+        dfs(0, -1, seats, adj);
         return res;
-    }
-
-private:
-    int dfs(int node, int parent, int seats) {
-        int passengers = 0;
-        for (int child : adj[node]) {
-            if (child != parent) {
-                int p = dfs(child, node, seats);
-                passengers += p;
-                res += ceil((double) p / seats);
-            }
-        }
-        return passengers + 1;
     }
 };
