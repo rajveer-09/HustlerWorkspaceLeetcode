068f472400b25bfab699028484531108e0485183
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-04-22 02:16:35 +0530

    Time: 21 ms (72.08%) | Memory: 73.6 MB (78.17%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -2,34 +2,36 @@ class Solution {
 public:
     int ds(int n) {
         int sum = 0;
-
         while (n) {
             sum += n % 10;
             n /= 10;
         }
-
         return sum;
     }
 
     int maximumSum(vector<int>& nums) {
-        map<int, priority_queue<int, vector<int>, greater<int>>> mpp;
+        unordered_map<int, pair<int, int>> mpp;
 
-        for (int num : nums) {
-            int sum = ds(num);
-            mpp[sum].push(num);
-
-            if (mpp[sum].size() > 2) {
-                mpp[sum].pop();
-            }
-        }
+// first → max1 → the largest number seen so far for that digit sum
+//second → max2 → the second largest number seen so far
 
         int maxi = -1;
 
-        for (auto& it : mpp) {
-            if (it.second.size() >= 2) {
-                int a = it.second.top(); it.second.pop();
-                int b = it.second.top();
-                maxi = max(maxi, a + b);
+        for (int num : nums) {
+            int sum = ds(num);
+
+            auto& p = mpp[sum];
+
+            if (num > p.first) {
+                p.second = p.first;
+                p.first = num;
+            } 
+            else if (num > p.second) {
+                p.second = num;
+            }
+
+            if (p.first && p.second) {
+                maxi = max(maxi, p.first + p.second);
             }
         }
 
