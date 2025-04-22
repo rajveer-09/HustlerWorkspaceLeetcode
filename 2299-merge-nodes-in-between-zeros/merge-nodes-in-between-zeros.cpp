1ed2abf9e526c32d85928dade861e9eefc92cb09
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-04-22 12:28:58 +0530

    Time: 57 ms (43.47%) | Memory: 295.3 MB (18.70%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -1,35 +1,27 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* modify = head->next; // Start from the node after the initial 0
-        ListNode* nextSum = modify;
+        ListNode* dummy = new ListNode(-1);
+        ListNode* curr = dummy;
+        ListNode* temp = head->next;
+        int sum = 0;
 
-        while (nextSum != nullptr) {
-            int sum = 0;
-            // Find the sum of all nodes until you encounter a 0.
-            while (nextSum->val != 0) {
-                sum += nextSum->val;
-                nextSum = nextSum->next;
+        while(temp != nullptr){
+            if(temp->val == 0){
+                if(sum != 0){
+                    ListNode* newNode = new ListNode(sum);
+                    curr->next = newNode;
+                    curr = curr->next;
+                    sum = 0;
+                }
+            }
+            else{
+                sum += temp->val;
             }
 
-            // Assign the sum to the current node's value.
-            modify->val = sum;
-            // Move nextSum to the first non-zero value of the next block.
-            nextSum = nextSum->next;
-            // Move modify also to this node.
-            modify->next = nextSum;
-            modify = modify->next;
+            temp = temp->next;
         }
-        return head->next; // Skip the initial 0 node.
+
+        return dummy->next;
     }
 };
