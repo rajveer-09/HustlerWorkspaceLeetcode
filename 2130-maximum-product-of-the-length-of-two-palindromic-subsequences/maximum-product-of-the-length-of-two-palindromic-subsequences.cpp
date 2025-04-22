6b75f9fb9546c97af6183bd64c57b6acf12a3a88
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-04-22 18:09:32 +0530

    Time: 1407 ms (24.57%) | Memory: 9 MB (62.12%) - LeetSync

--- 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
+++ 2130-maximum-product-of-the-length-of-two-palindromic-subsequences/maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
-    int lcs(string a, string b) {
-        int n = a.length();
-        int m = b.length();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    int ans = 0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (a[i - 1] == b[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+    bool isPalindrome(string str) {
+        int i = 0, j = str.size() - 1;
+        while (i < j) {
+            if (str[i] != str[j]) {
+                return false;
             }
+            i++;
+            j--;
         }
-        return dp[n][m];
+        return true;
     }
 
-    int answer(string s) {
-        string ss = s;
-        reverse(ss.begin(), ss.end());
-        return lcs(s, ss);
+    void dfs(int index, string s, string& s1, string& s2) {
+        if (index >= s.size()) {
+            if (isPalindrome(s1) && isPalindrome(s2)) {
+                int temp = s1.size() * s2.size();
+                ans = max(ans, temp);
+            }
+            return;
+        }
+
+        dfs(index + 1, s, s1, s2);
+
+        s1.push_back(s[index]);
+        dfs(index + 1, s, s1, s2);
+
+        s1.pop_back();
+
+        s2.push_back(s[index]);
+        dfs(index + 1, s, s1, s2);
+        s2.pop_back();
     }
 
     int maxProduct(string s) {
-        int n = s.length();
-        int ans = 0;
+        string s1 = "", s2 = "";
 
-        for (int i = 0; i < (1 << n); ++i) {
-            string x = "";
-            string y = "";
-            for (int j = 0; j < n; ++j) {
-                if ((i >> j) & 1) {
-                    x += s[j];
-                } else {
-                    y += s[j];
-                }
-            }
-            ans = max(ans, answer(x) * answer(y));
-        }
+        dfs(0, s, s1, s2);
+
         return ans;
     }
 };
