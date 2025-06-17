b7ace2fe5f6f2e375190169aa95cd8f8cdc0acb9
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-17 15:46:17 +0530

    Time: 1 ms (10.61%) | Memory: 23.4 MB (13.33%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.cpp
@@ -1,26 +1,16 @@
-#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
-    static TreeNode* removeLeafNodes(TreeNode* root, int target, TreeNode* parent=NULL) {
-        if (!root) return NULL;
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (!root) return nullptr;
 
-        // Recursively remove leaf nodes from left and right subtrees
-        root->left = removeLeafNodes(root->left, target, root);
-        root->right = removeLeafNodes(root->right, target, root);
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
 
-        // If current node is a leaf(not root) and its value==target, delete it
-        if (!root->left && !root->right && root->val==target) {
-            if (parent)  delete root; //the root cannot be deleted
-            return NULL;
+        // If this is a leaf node AND its value matches the target
+        if (!root->left && !root->right && root->val == target){
+            return nullptr;
         }
 
         return root;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
