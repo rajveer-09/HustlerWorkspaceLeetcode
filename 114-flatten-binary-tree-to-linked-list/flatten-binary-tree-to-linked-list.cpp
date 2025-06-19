e5bacaff35ed6513b9a2ea04d767e03560c864c4
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-19 23:09:01 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (77.06%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     TreeNode* prev = nullptr;
@@ -11,5 +12,32 @@ public:
         root->right = prev;
         root->left = nullptr;
         prev = root;
+    }
+};
+*/
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root) return;
+
+        TreeNode* curr =root;
+
+        while(curr){
+            if(curr->left){
+                TreeNode* temp = curr->left;
+
+                while(temp->right != NULL){
+                    temp = temp->right;
+                }
+
+                temp->right = curr->right;
+                curr->right = curr->left;
+                
+                curr->left = NULL;
+            }
+            
+            curr = curr->right;
+        }
+
     }
 };
