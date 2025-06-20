2f76a3ffd7d13ee024ce96d7b893b92ec326a76a
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-20 19:05:27 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (63.33%) - LeetSync

--- 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
+++ 1021-distribute-coins-in-binary-tree/distribute-coins-in-binary-tree.cpp
@@ -1,30 +1,18 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-        static int distributeCoins(TreeNode* root, TreeNode* parent= NULL) {
-        if (!root) return 0;
-        int  moves=distributeCoins(root->left, root)+distributeCoins(root->right, root);
-        int x=root->val-1;// give x coins to parent node
-        if (parent) parent->val += x; // now parent's coins
-        moves+=abs(x);
+    int moves = 0;
+    int dfs(TreeNode* node) {
+        if (!node) return 0;
+        int left = dfs(node->left);
+        int right = dfs(node->right);
+
+        moves += abs(left) + abs(right);  
+        return node->val + left + right - 1;  // excess coins to pass up
+    }
+    
+    int distributeCoins(TreeNode* root) {
+        moves = 0;
+        dfs(root);
         return moves;
     }
 };
-
-
-auto init = []() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
