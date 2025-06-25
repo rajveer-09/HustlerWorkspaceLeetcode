c68abb003026f24a0b71bc3f5757af818a6c6853
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-26 03:29:15 +0530

    Time: 34 ms (25.46%) | Memory: 13 MB (41.11%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-    int countSubstrings(string s) {
-        int n = s.length(), ans = 0;
-        for (int i = 0; i < n; ++i) {
-            int even = palindromeCount(s, i, i + 1);
-            int odd = palindromeCount(s, i, i);
-            ans += even + odd;
-        }
-        return ans;
+    int dp[1001][1001]; // -1 = unknown, 1 = palindrome, 0 = not palindrome
+    
+    bool isPalindrome(string &s, int i, int j) {
+        if (i >= j) return true;
+
+        if (dp[i][j] != -1) return dp[i][j];
+
+        if (s[i] != s[j]) return dp[i][j] = false;
+
+        return dp[i][j] = isPalindrome(s, i+1, j-1);
     }
 
-    int palindromeCount(const string& s, int left, int right) {
-        int count = 0;
-        while (left >= 0 && right < s.length() && s[left] == s[right]) {
-            --left;
-            ++right;
-            ++count;
+    int countSubstrings(string s) {
+        memset(dp, -1, sizeof(dp));
+
+        int n = s.size();
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                ans += isPalindrome(s, i, j);
+            }
         }
-        return count;
+
+        return ans;
     }
 };
