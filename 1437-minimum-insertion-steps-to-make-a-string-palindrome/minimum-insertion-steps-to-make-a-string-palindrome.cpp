cf3f5a6ac7b69b39e983e2787976e78d3d0e97fa
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-26 22:28:07 +0530

    Time: 46 ms (12.52%) | Memory: 13.5 MB (79.00%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int dp[1001][1001];
+    int solve(string& s1, string& s2, int i, int j){
+        if(i >= s1.size() || j >= s2.size()){
+            return 0;
+        }
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s1[i] == s2[j]){
+            return dp[i][j] = 1 + solve(s1, s2, i + 1, j + 1);
+        }
+
+        int op1 = solve(s1, s2, i + 1, j);
+        int op2 = solve(s1, s2, i, j + 1);
+
+        return dp[i][j] = max(op1, op2);
+    }
+    int longestPalindromeSubseq(string& s) {
+        memset(dp, -1, sizeof(dp));
+        string s2 = s;
+        reverse(s2.begin(), s2.end());
+
+        return solve(s, s2, 0, 0);
+    }
+    int minInsertions(string s) {
+       int n = longestPalindromeSubseq(s);
+
+       return s.size() - n;
+    }
+};
