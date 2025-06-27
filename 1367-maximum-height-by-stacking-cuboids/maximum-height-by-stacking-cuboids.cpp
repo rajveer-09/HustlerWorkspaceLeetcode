79a569e821204c5a524fb1cec40c69f45ef2a596
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-27 12:17:06 +0530

    Time: 5 ms (86.72%) | Memory: 13.2 MB (72.03%) - LeetSync

--- 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
+++ 1367-maximum-height-by-stacking-cuboids/maximum-height-by-stacking-cuboids.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     int maxHeight(vector<vector<int>>& cuboids) {
-        // Sort dimensions within each cuboid
-        for (auto& cuboid : cuboids) {
-            sort(cuboid.begin(), cuboid.end());
-        }
-        
-        // Sort all cuboids based on their dimensions
-        sort(cuboids.begin(), cuboids.end(), [](const vector<int>& a, const vector<int>& b) {
-            return tie(a[0], a[1], a[2]) < tie(b[0], b[1], b[2]);
-        });
-
         int n = cuboids.size();
-        vector<int> dp(n, 0);
-        int maxHeight = 0;
+
+        for (auto& c : cuboids) sort(c.begin(), c.end()); // sort dimensions
+        sort(cuboids.begin(), cuboids.end());             // sort cuboids
+
+        vector<int> dp(n);
+        int maxH = 0;
 
         for (int i = 0; i < n; ++i) {
-            dp[i] = cuboids[i][2];
+            dp[i] = cuboids[i][2]; // take current height
             for (int j = 0; j < i; ++j) {
-                if (cuboids[i][0] >= cuboids[j][0] &&
-                    cuboids[i][1] >= cuboids[j][1] &&
-                    cuboids[i][2] >= cuboids[j][2]) {
+                if (cuboids[j][0] <= cuboids[i][0] &&
+                    cuboids[j][1] <= cuboids[i][1] &&
+                    cuboids[j][2] <= cuboids[i][2]) {
                     dp[i] = max(dp[i], dp[j] + cuboids[i][2]);
                 }
             }
-            maxHeight = max(maxHeight, dp[i]);
+            maxH = max(maxH, dp[i]);
         }
 
-        return maxHeight;
+        return maxH;
     }
 };
