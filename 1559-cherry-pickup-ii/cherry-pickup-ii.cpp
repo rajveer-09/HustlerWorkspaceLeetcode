ae48cd211ef1d1a11be5e975df4aca08d50a5154
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-27 14:53:18 +0530

    Time: 18 ms (98.47%) | Memory: 13.7 MB (90.11%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,30 +1,35 @@
 class Solution {
-public:
-    int cherryPickup(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
-        dp[0][0][n-1] = grid[0][0] + grid[0][n-1];
+    public:
 
-        int ans = 0;
-        for(int i = 1; i < m; ++i) {
-            for(int j = 0; j < n; ++j) { // robotA
-                for(int k = j+1; k < n; ++k) { // robotB
-                    for(int x = -1; x <= 1; ++x) { // x and y all possible combinations
-                        for(int y = -1; y <= 1; ++y) {
-                            int nj = j + x, nk = k + y;
-                            if(nj >= 0 && nj < n && nk >= 0 && nk < n) {
-                                int prev = dp[i-1][nj][nk];
-                                if(prev != -1) {
-                                    dp[i][j][k] = max(dp[i][j][k], prev + grid[i][j] + (j != k ? grid[i][k] : 0));
-                                }
-                            }
-                        }
-                    }
-                    if(ans < dp[i][j][k]) ans = dp[i][j][k];
-                }
+    int dp[71][71][71];
+
+    int dfs(int row, int c1, int c2, vector<vector<int>>& grid, int m, int n) {
+        if (c1 < 0 || c1 >= n || c2 < 0 || c2 >= n) return INT_MIN;
+
+        if (row == m - 1){
+            return grid[row][c1] + (c1 == c2 ? 0 : grid[row][c2]);
+        }
+
+        if (dp[row][c1][c2] != -1) return dp[row][c1][c2];
+
+        int maxCherries = 0;
+
+        for (int move1 = -1; move1 <= 1; move1++) {
+            for (int move2 = -1; move2 <= 1; move2++) {
+                int val = dfs(row + 1, c1 + move1, c2 + move2, grid, m, n);
+                maxCherries = max(maxCherries, val);
             }
         }
-        
-        return ans;
+
+        int cherries = grid[row][c1] + (c1 == c2 ? 0 : grid[row][c2]);
+
+        return dp[row][c1][c2] = cherries + maxCherries;
+    }
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        memset(dp, -1, sizeof(dp));
+
+        return dfs(0, 0, n - 1, grid, m, n);
     }
 };
