c59c3e7efd10bcb0a0f381f82a02d15e7c15d454
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-27 14:19:05 +0530

    Time: 3 ms (15.98%) | Memory: 13.3 MB (30.93%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    int func(int i, int j, vector<vector<int> > &triangle, vector<vector<int>> &dp) {
-        int n = triangle.size();
-
-        if (i == n - 1) return triangle[i][j]; 
-
-        if(dp[i][j] != INT_MAX) return dp[i][j];
-
-        int down = triangle[i][j] + func(i + 1, j, triangle, dp);
-
-        int diagonal = triangle[i][j] + func(i + 1, j + 1, triangle, dp);
-
-        return dp[i][j] = min(down, diagonal);
-   }
     int minimumTotal(vector<vector<int>>& triangle) {
-        ios_base::sync_with_stdio(false);
         int n = triangle.size();
-        vector<vector<int> > dp(n, vector<int>(n, INT_MAX));
-        return func(0, 0, triangle, dp);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        // Base case: fill the last row as is
+        for (int j = 0; j < triangle[n - 1].size(); ++j) {
+            dp[n - 1][j] = triangle[n - 1][j];
+        }
+
+        // Build from bottom up
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = 0; j < triangle[i].size(); ++j) {
+                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
+            }
+        }
+
+        // Top element is the answer
+        return dp[0][0];
     }
 };
+
+
