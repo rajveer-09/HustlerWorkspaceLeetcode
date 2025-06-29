299e1b5fff6c6e73a50ae469a9c9f69e60b95dcc
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-06-29 16:40:28 +0530

    Time: 3 ms (30.49%) | Memory: 17.1 MB (34.45%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,56 +1,49 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>> &grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-       
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-        queue<pair<pair<int, int>, int>> rottenQueue;
-        
-        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        queue<pair<int, int>> q;
 
-        // Enqueue all initially rotten oranges
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 2) {
-                    rottenQueue.push({{i, j}, 0});
-                    visited[i][j] = true;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 2){
+                    q.push({i, j});
                 }
             }
         }
 
-        int maxTime = 0;
+        int cnt = 0;
+        vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
 
-        // Perform BFS to rot adjacent fresh oranges
-        while (!rottenQueue.empty()) {
-            int currentRow = rottenQueue.front().first.first;
-            int currentCol = rottenQueue.front().first.second;
-            int time = rottenQueue.front().second;
+        while(!q.empty()){
+            int sz = q.size();
+            bool changed = false;
 
-            maxTime = max(maxTime, time);
-            rottenQueue.pop();
+            for(int i = 0; i < sz; i++){
+                auto [x, y] = q.front(); q.pop();
 
-            // Check all four possible directions
-            for (auto direction : directions) {
-                int newRow = currentRow + direction.first;
-                int newCol = currentCol + direction.second;
+                for(auto& d : dir){
+                    int xx = x + d[0];
+                    int yy = y + d[1];
 
-                if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
-                    if (!visited[newRow][newCol] && grid[newRow][newCol] == 1) {
-                        rottenQueue.push({{newRow, newCol}, time + 1});
-                        visited[newRow][newCol] = true;
+                    if(xx >= 0 && xx < n && yy >= 0 && yy < m && grid[xx][yy] == 1){
+                        grid[xx][yy] = 2;
+                        q.push({xx, yy});
+                        changed = true;
                     }
                 }
             }
+
+            if(changed) cnt++;
         }
 
-        // Check if there's any fresh orange left unrotted
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (!visited[i][j] && grid[i][j] == 1) return -1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == 1) return -1;
             }
         }
 
-        return maxTime;
+        return cnt;
     }
 };
