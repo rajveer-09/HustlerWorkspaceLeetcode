173b77108547b53b7e6437eb0cf22c509a6b3403
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-07-04 04:06:57 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (68.60%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -1,40 +1,39 @@
 class Solution {
-public:
-    string reverseWords(string s) {
-
-        int start = 0, end = s.size() - 1;
-        while (start <= end && s[start] == ' ') start++;
-        while (end >= start && s[end] == ' ') end--;
-        
-       
-        string reversed = "";
-        for (int i = end; i >= start; i--) {
-            reversed += s[i];
-        }
-    
-        int n = reversed.size();
-        int left = 0, right = 0;
-        while (right < n) {
-            while (right < n && reversed[right] != ' ') right++;
-            reverseWord(reversed, left, right - 1);
-            right++;
-            left = right;
-        }
-        
-        string result = "";
-        for (int i = 0; i < n; i++) {
-            if (reversed[i] != ' ' || (result.back() != ' ')) {
-                result += reversed[i];
-            }
-        }
-
-        return result;
-    }
-    
 private:
-    void reverseWord(string& s, int start, int end) {
+    void reverseString(string &s, int start, int end) {
         while (start < end) {
             swap(s[start++], s[end--]);
         }
+    }
+
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        reverseString(s, 0, n - 1);
+
+        int i = 0, j = 0, start = 0, end = 0;
+
+        while (j < n) {
+            while (j < n && s[j] == ' ') j++;
+            if (j == n) break;
+
+            start = i;
+
+            while (j < n && s[j] != ' ') {
+                s[i++] = s[j++];
+            }
+
+            end = i - 1;
+            reverseString(s, start, end);
+
+            // if (j < n) {
+            //     s[i++] = ' ';
+            // }
+            s[i++] = ' ';
+        }
+
+        if (i > 0 && s[i - 1] == ' ') i--;
+
+        return s.substr(0, i);
     }
 };
