74c9e9273bfc0516852768bfc1b1e3bccdc520bb
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-07-05 02:36:52 +0530

    Time: 9 ms (92.87%) | Memory: 47.1 MB (79.42%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,42 +1,55 @@
-using ll = long long;
-const int MOD = 1e9 + 7;
 class Solution {
+private:
+    vector<int> findNSE(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+        for(int i = n - 1; i >= 0; i--) {
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            ans[i] = !st.empty() ? st.top() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+    
+    vector<int> findPSEE(vector<int> &arr) {
+        int n = arr.size();
+        vector<int> ans(n);
+        stack<int> st;
+
+        for(int i=0; i < n; i++) {
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
+            }
+            ans[i] = !st.empty() ? st.top() : -1;
+            st.push(i);
+        }
+
+        return ans;
+    }
+    
 public:
-    int sumSubarrayMins(vector<int>& nums) {
-        int length = nums.size();
-        vector<int> left(length, -1);
-        vector<int> right(length, length);
-        stack<int> stk;
+    int sumSubarrayMins(vector<int> &arr) {
+        vector<int> nse = findNSE(arr);
+        vector<int> psee = findPSEE(arr);
 
-        for (int i = 0; i < length; ++i) {
-            while (!stk.empty() && nums[stk.top()] >= nums[i]) {
-                stk.pop();
-            }
-            if (!stk.empty()) {
-                left[i] = stk.top();
-            }
-            stk.push(i);
+        int n = arr.size();
+        int mod = 1e9 + 7;
+        int sum = 0;
+
+        for(int i=0; i < n; i++) {
+            int left = i - psee[i];
+            int right = nse[i] - i;
+
+            long long freq = left * right * 1LL;
+
+            int val = (freq * arr[i] * 1LL) % mod;
+
+            sum = (sum + val) % mod;
         }
-
-        stk = stack<int>();
-
-        for (int i = length - 1; i >= 0; --i) {
-            while (!stk.empty() && nums[stk.top()] > nums[i]) {
-                stk.pop();
-            }
-            if (!stk.empty()) {
-                right[i] = stk.top();
-            }
-            stk.push(i);
-        }
-
-        ll sum = 0;
-
-        for (int i = 0; i < length; ++i) {
-            sum += static_cast<ll>(i - left[i]) * (right[i] - i) * nums[i] % MOD;
-            sum %= MOD;
-        }
-
+        
         return sum;
     }
 };
