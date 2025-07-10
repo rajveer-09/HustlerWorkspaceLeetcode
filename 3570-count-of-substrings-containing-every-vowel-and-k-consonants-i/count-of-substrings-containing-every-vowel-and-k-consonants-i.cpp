333c608129997c75edd9be7e3bd8efbee279c3f1
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-07-10 11:23:59 +0530

    Time: 2 ms (86.19%) | Memory: 10.4 MB (62.15%) - LeetSync

---
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    long long atLeast(string& s, int k, int n) {
+        unordered_map<char, int> vow = {{'a',1}, {'e',1}, {'i',1}, {'o',1}, {'u',1}};
+        int st = 0, cntVow = 0, cntCons = 0;
+        long long ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (vow.find(s[i]) == vow.end()) {
+                cntCons++;
+            } else {
+                vow[s[i]]--;
+                if (vow[s[i]] == 0) cntVow++;
+            }
+
+            while (st <= i && cntVow == 5 && cntCons >= k) {
+                ans += (n - i);
+
+                if (vow.find(s[st]) == vow.end()) {
+                    cntCons--;
+                } else {
+                    vow[s[st]]++;
+                    if (vow[s[st]] == 1) cntVow--;
+                }
+
+                st++;
+            }
+        }
+
+        return ans;
+    }
+
+    long long countOfSubstrings(string word, int k) {
+        int n = word.size();
+        
+        return atLeast(word, k, n) - atLeast(word, k + 1, n);
+    }
+};
+/*
+-> atLeast(k) → all substrings with k ya usse zyada consonants
+-> atLeast(k + 1) → all substrings with k+1 ya usse zyada consonants
+
+Toh in dono ka difference hi hoga exactly k consonants wale
+
+*/
