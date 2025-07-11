cb074d2757ed3d8c0d5fd4d23bd6ee2bb14e4ebd
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-07-11 16:24:34 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (37.89%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
-    ListNode* reverse(ListNode* head) {
-        ListNode* prev = nullptr;
-
-        while (head) {
-            ListNode* next = head->next;
-            head->next = prev;
-            prev = head;
-            head = next;
-        }
-
-        return prev;
-    }
-
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         if (!head || left == right) return head;
 
-        ListNode dummy(0);
-        dummy.next = head;
-        ListNode* prev = &dummy;
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+
+        ListNode* prev = dummy;
+
 
         for (int i = 1; i < left; ++i) {
             prev = prev->next;
         }
 
-        ListNode* start = prev->next;
-        ListNode* end = start;
+        ListNode* curr = prev->next;
+
+        // har aane waale node ko prev se jodtey jaao....
+        // means..prev -> 3 -> 2 -> 4
+        // then prev -> 4 -> 3 -> 2
+        // jab tak curr yani 2 pr ye operation (r -l) times na ho jaaye
+
         for (int i = 0; i < right - left; ++i) {
-            end = end->next;
+            ListNode* temp = curr->next;
+            // temp curr ke aage wala node
+            curr->next = temp->next;
+            // jab temp hatatey to curr ko temp ke aage wale se connect maaro
+            
+            temp->next = prev->next;
+            // temp ke next ko prev ke aage waale se connect maaro bich me lana hai
+            prev->next = temp;
+            // temp ko prev ke aage laao
         }
 
-        // Detach and reverse the sublist
-        ListNode* next = end->next;
-        end->next = nullptr;
-
-        prev->next = reverse(start);
-        start->next = next;
-
-        return dummy.next;
+        return dummy->next;
     }
 };
