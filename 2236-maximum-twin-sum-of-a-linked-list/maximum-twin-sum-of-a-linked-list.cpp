a0fd47d7174f1854cc68797b7a1e2551c5831b05
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-07-11 15:11:36 +0530

    Time: 4 ms (58.96%) | Memory: 124.5 MB (57.14%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,52 +1,45 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
- ListNode* Middle(ListNode* head)
- {
-    ListNode* slow = head;
-    ListNode* fast = head;
-    while(fast)
-    {
-        slow=slow->next;
-        fast=fast->next->next;
-    }
-    return slow;
- }
- ListNode*Reverse(ListNode* head)
- {
-    ListNode* prev = NULL;
-    ListNode* cur = head;
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
 
-    while(cur)
-    {
-        ListNode* temp = cur->next;
-        cur->next = prev;
-        prev = cur;
-        cur = temp;
-    }
-    return prev;
+        while(head){
+            ListNode* nxt = head->next;
 
- }
+            head->next = prev;
+            prev = head;
+            head = nxt;
+        }
+
+        return prev;
+    }
+    ListNode* middleNode(ListNode* head){
+        ListNode* slow= head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
     int pairSum(ListNode* head) {
-    ListNode* newhead = Middle(head);
-    newhead = Reverse(newhead);
-    int ans = 0;
-    while(newhead && head)
-    {
-          ans = max(ans,(newhead->val+head->val));
-          newhead = newhead->next;
-          head = head->next;
-    }
 
-        return ans;
+        ListNode* mid = middleNode(head);
+        ListNode* second = reverse(mid);
+
+        cout<<"second "<<second->val<<endl;
+
+        int maxi = 0;
+
+        while(second){
+            maxi = max(head->val + second->val, maxi);
+
+            head = head->next;
+            second = second->next;
+        }
+
+        return maxi;
     }
 };
