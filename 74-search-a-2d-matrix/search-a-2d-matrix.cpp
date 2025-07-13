17d4ce5c3c1754c54aa2213f78caeb06984a77e2
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-07-13 22:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (97.83%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    pair<int, int> find_pos(int k, int n) {
-        pair<int, int> z;
-        z.first = k / n;
-        z.second = k % n;
-
-        return z;
-    }
-
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(), n = matrix[0].size();
-        int l  = -1, r = n * m;
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        while(r - l > 1) {
-            int mid = l + (r - l) / 2;
-            int x = mid / n, y = mid % n;
+        int st = 0, ed = m * n - 1;
 
-            if(matrix[x][y] == target) {
+        while (st <= ed) {
+            int mid = st + (ed - st) / 2;
+            int val = matrix[mid / m][mid % m];
+
+            if (val > target) {
+                ed = mid - 1;
+            } 
+            else if (val < target) {
+                st = mid + 1;
+            } 
+            else {
                 return true;
             }
-            
-            if(matrix[x][y] > target) {
-                r = mid;
-            }
-
-            else {
-                l = mid;
-            }
-
         }
 
-        return false; 
+        return false;
     }
 };
