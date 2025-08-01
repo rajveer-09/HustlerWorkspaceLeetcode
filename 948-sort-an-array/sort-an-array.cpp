81b87ed606b3b6a65673e28be9e6fc698965eec4
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-01 14:01:38 +0530

    Time: 569 ms (38.07%) | Memory: 295.7 MB (17.52%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,20 +1,51 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        vector<int> counting(2 * 50000 + 1, 0);
-        for (int num : nums) {
-            // we add 5 * 10^4 because for smallest possible element -5 * 10^4 index must be 0
-            counting[num + 50000]++;
-        }
-        int write_ind = 0;
-        for (int number_ind = 0; number_ind < counting.size(); ++number_ind) {
-            int freq = counting[number_ind];
-            while (freq != 0) {
-                nums[write_ind] = number_ind - 50000;
-                write_ind++;
-                freq--;
+    void merge(vector<int>& nums, int i, int j){
+        int mid = i + (j - i) / 2;
+        int pt1 = i;
+        int pt2 = mid + 1;
+
+        vector<int> ans;
+
+        while(pt1 <= mid && pt2 <= j){
+            if(nums[pt1] <= nums[pt2]){
+                ans.push_back(nums[pt1]);
+                pt1++;
+            }
+            else{
+                ans.push_back(nums[pt2]);
+                pt2++;
             }
         }
+
+        while(pt1 <= mid){
+            ans.push_back(nums[pt1]);
+            pt1++;
+        }
+        while(pt2 <= j){
+            ans.push_back(nums[pt2]);
+            pt2++;
+        }
+
+        for(int k = j; k >= i; k--){
+            nums[k] = ans.back();
+            ans.pop_back();
+        }
+    }
+    void mergeSort(vector<int>& nums, int i, int j){
+        if(i >= j) return;
+        
+        int mid = i + (j - i) / 2;
+
+        mergeSort(nums, i, mid);
+        mergeSort(nums, mid + 1, j);
+
+        merge(nums, i, j);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        mergeSort(nums, 0, n - 1);
+
         return nums;
     }
 };
