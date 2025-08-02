db18a212bd6128a02bf79b8adfce504b0be40f50
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-02 11:04:25 +0530

    Time: 8 ms (92.19%) | Memory: 57 MB (90.60%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,32 +1,49 @@
 class Solution {
 public:
+    ListNode* merge(ListNode* L1, ListNode* L2) {
+        ListNode dummy(0);
+        ListNode* temp = &dummy;
+
+        while (L1 && L2) {
+            if (L1->val <= L2->val) {
+                temp->next = L1;
+                L1 = L1->next;
+            } else {
+                temp->next = L2;
+                L2 = L2->next;
+            }
+            temp = temp->next;
+        }
+
+        temp->next = L1 ? L1 : L2;
+        return dummy.next;
+    }
+
+    ListNode* middle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    ListNode* mergeSort(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        ListNode* mid = middle(head);
+        ListNode* rightHead = mid->next;
+        mid->next = nullptr;
+
+        ListNode* L1 = mergeSort(head);
+        ListNode* L2 = mergeSort(rightHead);
+
+        return merge(L1, L2);
+    }
+
     ListNode* sortList(ListNode* head) {
-        if(head == nullptr or head->next == nullptr) return head;
-
-        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, greater<pair<int, ListNode*>>> pq;
-
-        ListNode* curr = head;
-
-        while(curr!=nullptr){
-            pq.push({curr->val,curr});
-            curr = curr->next;
-        }
-
-        pair<int ,ListNode*> p = pq.top();
-        head = p.second;
-
-        curr = head;
-        pq.pop();
-
-        while(!pq.empty()){
-            p = pq.top();
-            curr->next = p.second;
-            
-            curr = curr->next;
-            pq.pop();
-        }
-
-        curr->next = nullptr;
-        return head;
+        return mergeSort(head);
     }
 };
