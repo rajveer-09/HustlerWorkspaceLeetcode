1d4d67e9dafa9e10c47bb6f629763e9ebcd8e4fc
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-02 11:19:14 +0530

    Time: 1 ms (16.15%) | Memory: 15.9 MB (20.30%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(!head || !head->next) return head;
+        ListNode* dummy = new ListNode(0);
 
-        ListNode* dummy = new ListNode(-101);
-        ListNode* temp = dummy;
+        ListNode* prev = dummy; // yaha tak no duplicate
+        prev->next = head;
+        ListNode* curr = head;
 
-        ListNode* curr = head->next;
-        ListNode* prev = head;
+        while (curr) {
+            if (curr->next && curr->val == curr->next->val) {
+                int duplicateVal = curr->val;
+                while (curr && curr->val == duplicateVal) {
+                    ListNode* temp = curr;
+                    curr = curr->next;
+                    delete temp;
+                }
+                prev->next = curr;
+            } else {
 
-        if(head->next->val != head->val){
-            temp->next = head;
-            temp = temp->next;
-            // cout<<"...."<<endl;
-        }
-
-        while(curr->next != NULL){
-            if(curr->val != prev->val && curr->val != curr->next->val){
-                // cout<<".."<<endl;
-                temp->next = curr;
-                temp = temp->next;
+                prev = curr;
+                curr = curr->next;
             }
-
-            // cout<<"."<<endl;
-
-            prev = curr;
-            curr = curr->next;
         }
-
-        if(curr->val != prev->val){
-            // cout<<"..."<<endl;
-            temp->next = curr;
-            temp = temp->next;
-        }
-        temp->next = NULL;
 
         return dummy->next;
     }
