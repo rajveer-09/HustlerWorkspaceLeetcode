29b99c5f7885187a92ced5800c01eb3c20a31407
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-05 00:36:39 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (52.82%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-    vector<string> findRelativeRanks(vector<int>& score) {
-        int n = score.size();
-        int max_s = 0;
+    vector<string> findRelativeRanks(vector<int>& score) {
+        int n = score.size();
+        int maxi = -1;
 
-        for (int s : score) {
-            max_s = max(max_s, s);
-        }
+        for (int num : score) {
+            maxi = max(maxi, num);
+        }
 
-        vector<int> s_map(max_s + 1, -1);
-        for (int i = 0; i < n; ++i) {
-            s_map[score[i]] = i;
-        }
+        vector<int> map(maxi + 1, -1);
+        for (int i = 0; i < n; i++) {
+            map[score[i]] = i;
+        }
 
-        vector<string> ans(n);
-        int rank = 1;
+        vector<string> ans(n);
+        int rank = 1;
 
-        for (int s = max_s; s >= 0; --s) {
-            if (s_map[s] != -1) {
-                int idx = s_map[s];
-                if (rank == 1) {
-                    ans[idx] = "Gold Medal";
-                } else if (rank == 2) {
-                    ans[idx] = "Silver Medal";
-                } else if (rank == 3) {
-                    ans[idx] = "Bronze Medal";
-                } else {
-                    ans[idx] = to_string(rank);
-                }
-                rank++;
-            }
-        }
-        return ans;
-    }
+        for (int i = maxi; i >= 0; i--) {
+            if (map[i] != -1) {
+                int idx = map[i];
+                
+                if (rank == 1) {
+                    ans[idx] = "Gold Medal";
+                } 
+                else if (rank == 2) {
+                    ans[idx] = "Silver Medal";
+                } 
+                else if (rank == 3) {
+                    ans[idx] = "Bronze Medal";
+                } 
+                else {
+                    ans[idx] = to_string(rank);
+                }
+
+                rank++;
+
+                if (rank > n) break;
+
+            }
+        }
+
+        return ans;
+    }
 };
