73a977fe051880920913e20c151937bb82d782e7
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-09 10:12:25 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (72.39%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int maxAreaOfIsland(vector<vector<int>>& grid) {
-        int maxArea = 0;
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
+    void dfs(vector<vector<int>>& grid, int i, int j, int& count) {
+        int m = grid.size();
+        int n = grid[0].size();
+        
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            return;
+        }
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (grid[i][j] == 1 && !visited[i][j]) {
-                    int currentArea = dfs(grid, visited, i, j);
-                    maxArea = max(maxArea, currentArea);
+        grid[i][j] = 0;
+        count++;
+
+        dfs(grid, i + 1, j, count);
+        dfs(grid, i - 1, j, count);
+        dfs(grid, i, j + 1, count);
+        dfs(grid, i, j - 1, count);
+    }
+
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int maxA = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    int area = 0;
+                    dfs(grid, i, j, area);
+                    maxA = max(maxA, area);
                 }
             }
         }
-
-        return maxArea;
-    }
-
-private:
-    int dfs(vector<vector<int>>& grid, vector<vector<bool>>& visited, int i, int j) {
-        int n = grid.size();
-        int m = grid[0].size();
-
-        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0 || visited[i][j]) {
-            return 0;
-        }
-
-        visited[i][j] = true;
-        int area = 1;
-
-        // Perform DFS in all four possible directions
-        area += dfs(grid, visited, i - 1, j); // up
-        area += dfs(grid, visited, i + 1, j); // down
-        area += dfs(grid, visited, i, j - 1); // left
-        area += dfs(grid, visited, i, j + 1); // right
-
-        return area;
+        return maxA;
     }
 };
