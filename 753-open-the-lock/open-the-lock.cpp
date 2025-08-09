565f3faf9bb088e7b003671f970d77450c17af5c
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-09 21:24:48 +0530

    Time: 123 ms (74.89%) | Memory: 38.5 MB (83.23%) - LeetSync

--- 753-open-the-lock/open-the-lock.cpp
+++ 753-open-the-lock/open-the-lock.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
-        unordered_set<string> deadendSet(deadends.begin(), deadends.end());
-        if (deadendSet.count("0000")) {
+        unordered_set<string> deads(deadends.begin(), deadends.end());
+        if (deads.count("0000")) {
             return -1;
         }
-        
-        queue<pair<string, int>> queue;
-        queue.push({"0000", 0});
+
+        queue<string> q;
+        q.push("0000");
         unordered_set<string> visited;
         visited.insert("0000");
         
-        while (!queue.empty()) {
-            auto current = queue.front();
-            queue.pop();
-            string currentCombination = current.first;
-            int moves = current.second;
-            
-            if (currentCombination == target) {
-                return moves;
-            }
-            
-            for (int i = 0; i < 4; i++) {
-                for (int delta : {-1, 1}) {
-                    int newDigit = (currentCombination[i] - '0' + delta + 10) % 10;
-                    string newCombination = currentCombination;
-                    newCombination[i] = '0' + newDigit;
-                    
-                    if (visited.find(newCombination) == visited.end() && deadendSet.find(newCombination) == deadendSet.end()) {
-                        visited.insert(newCombination);
-                        queue.push({newCombination, moves + 1});
+        int moves = 0;
+        while (!q.empty()) {
+            int levelSize = q.size();
+            while (levelSize--) {
+                string curr = q.front();
+                q.pop();
+
+                if (curr == target) {
+                    return moves;
+                }
+
+                for (int i = 0; i < 4; ++i) {
+                    for (int d = -1; d <= 1; d += 2) {
+                        string next = curr;
+                        next[i] = (curr[i] - '0' + d + 10) % 10 + '0';
+                        
+                        if (visited.find(next) == visited.end() && deads.find(next) == deads.end()) {
+                            visited.insert(next);
+                            q.push(next);
+                        }
                     }
                 }
             }
+            
+            moves++;
         }
         
-        return -1; // Target is not reachable
+        return -1;
     }
 };
