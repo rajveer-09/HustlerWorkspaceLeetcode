c0f609533e1995f3274c1851e88ac7ba0a4efdd0
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-10 11:59:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (95.83%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    std::string makeGood(std::string s) {
-        std::stack<char> stack;
+    string makeGood(string s) {
+        string res = "";
         
         for (char c : s) {
-            if (!stack.empty() && std::abs(c - stack.top()) == 32) {
-                stack.pop();
-            } else {
-                stack.push(c);
+            if (!res.empty() && abs(c - res.back()) == 32) {
+                res.pop_back();
+            } 
+            else {
+                res.push_back(c);
             }
         }
-        
-        std::string result;
-        while (!stack.empty()) {
-            result = stack.top() + result;
-            stack.pop();
-        }
-        
-        return result;
+        return res;
     }
 };
