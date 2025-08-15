a7358f524d7828b72fce56181cfbf534645ddbb3
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-15 18:00:09 +0530

    Time: 15 ms (62.93%) | Memory: 26.7 MB (81.91%) - LeetSync

--- 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -1,46 +1,47 @@
-
 class Solution {
 public:
-    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
-        unordered_map<int, TreeNode*> res;
-        unordered_set<int> to_delete_set(to_delete.begin(), to_delete.end());
-        res[root->val] = root;
-
-        function<void(TreeNode*, TreeNode*, bool)> recursion = [&](TreeNode* parent, TreeNode* cur_node, bool isleft) {
-            if (cur_node == nullptr) return;
-
-            recursion(cur_node, cur_node->left, true);
-            recursion(cur_node, cur_node->right, false);
-
-            if (to_delete_set.find(cur_node->val) != to_delete_set.end()) {
-                if (res.find(cur_node->val) != res.end()) {
-                    res.erase(cur_node->val);
-                }
-
-                if (parent) {
-                    if (isleft) {
-                        parent->left = nullptr;
-                    } else {
-                        parent->right = nullptr;
-                    }
-                }
-
-                if (cur_node->left) {
-                    res[cur_node->left->val] = cur_node->left;
-                }
-                if (cur_node->right) {
-                    res[cur_node->right->val] = cur_node->right;
-                }
-            }
-        };
-
-        recursion(nullptr, root, false);
-        
-        vector<TreeNode*> result;
-        for (auto& pair : res) {
-            result.push_back(pair.second);
+    TreeNode* deleteHelper(TreeNode* root, unordered_set<int>& st, vector<TreeNode*>& result) {
+        if (root == NULL) {
+            return NULL;
         }
-        
+
+        root->left  = deleteHelper(root->left, st, result);
+        root->right = deleteHelper(root->right, st, result);
+
+        /* agr node ko delete kiye toh uske direct left and right
+           nodes bachenge jo ki new forest ke root honge  */
+
+        if (st.find(root->val) != st.end()) {
+            if (root->left != NULL) {
+                result.push_back(root->left);
+            }
+
+            if (root->right != NULL) {
+                result.push_back(root->right);
+            }
+
+            return NULL;
+        } 
+        else {
+            return root;
+        }
+    }
+
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        vector<TreeNode*> result;
+
+        unordered_set<int> st;
+
+        for (int &num : to_delete) {
+            st.insert(num);
+        }
+
+        deleteHelper(root, st, result);
+        /* Agar root ko delete nhi krna toh vo v ek forest banayega  */
+        if (st.find(root->val) == st.end()) {
+            result.push_back(root);
+        }
+
         return result;
     }
 };
