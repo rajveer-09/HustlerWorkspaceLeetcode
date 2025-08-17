317cc995165e5a21690fe8762176136b176a1e60
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-17 15:09:21 +0530

    Time: 3 ms (70.16%) | Memory: 31.9 MB (67.80%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -1,28 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ * int val;
+ * TreeNode *left;
+ * TreeNode *right;
+ * TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ * TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ * TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class BSTIterator {
-public:
-    vector<int> values;
-    int index;
-
-    void inorderTraversal(TreeNode* node) {
-        if (!node) return;
-        inorderTraversal(node->left);
-
-        values.push_back(node->val);
-        
-        inorderTraversal(node->right);
+private:
+    stack<TreeNode*> myStack;
+    void pushAll(TreeNode* node) {
+        while (node) {
+            myStack.push(node);
+            node = node->left;
+        }
     }
-    
+public:
     BSTIterator(TreeNode* root) {
-        inorderTraversal(root);
-        index = -1;
+        pushAll(root);
     }
     
     bool hasNext() {
-        return index + 1 < values.size();
+        return !myStack.empty();
     }
     
     int next() {
-        return values[++index];
+        TreeNode* temp = myStack.top();
+        myStack.pop();
+        pushAll(temp->right);
+        return temp->val;
     }
-
 };
