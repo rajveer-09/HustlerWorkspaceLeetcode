eef5bd74058cb1e65b41bd8eb73ce8e0ba63810c
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-18 11:43:42 +0530

    Time: 355 ms (5.05%) | Memory: 30.6 MB (50.32%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    bool memo(vector<int>& arr, int idx, int sum, vector<vector<int>>& dp) {
-        // Base Case
-        if (sum == 0) return true;
-        if (idx >= arr.size() || sum < 0) return false;
-        
-        if (dp[sum][idx] != -1) return dp[sum][idx];
-        
-        bool inc = memo(arr, idx + 1, sum - arr[idx], dp);
-        bool exc = memo(arr, idx + 1, sum, dp);
-        
-        return dp[sum][idx] = (inc || exc);
-    }
-    bool canPartition(vector<int>& arr) {
-        ios_base::sync_with_stdio(false);
-        int sum = accumulate(arr.begin(), arr.end(), 0);
-        if(sum %2 !=0) return false;
-        else sum = sum/2;
+    int dp[201][20001];
 
-        int n = arr.size();
-        vector<vector<int>> dp(sum + 1, vector<int>(n, -1));
-        
-        return memo(arr, 0, sum, dp);
+    bool solve(vector<int>& nums, int idx, int tsum, int sum) {
+        int n = nums.size();
+        if((tsum - sum) == sum) return true;
+
+        if (idx >= n) {
+            return false;
+        }
+
+        if (dp[idx][sum] != -1) return dp[idx][sum];
+
+        bool take = solve(nums, idx + 1, tsum, sum + nums[idx]);
+        bool not_take = solve(nums, idx + 1, tsum, sum);
+
+        return dp[idx][sum] = (take || not_take);
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int tsum = accumulate(nums.begin(), nums.end(), 0);
+        memset(dp, -1, sizeof(dp));
+
+        return solve(nums, 0, tsum, 0);
     }
 };
