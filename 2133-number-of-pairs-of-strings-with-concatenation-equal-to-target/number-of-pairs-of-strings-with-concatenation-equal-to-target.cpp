88ced7d5bfd5683872e737900784402d4ef39875
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-19 11:31:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (93.72%) - LeetSync

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
+        unordered_map<string, int> freq;
+        for (const string& s : nums) {
+            freq[s]++;
+        }
+
         int count = 0;
         int n = nums.size();
 
         for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i == j) {
-                    continue;
-                }
-                if (nums[i] + nums[j] == target) {
-                    count++;
+            if (target.size() > nums[i].size()) {
+                if (target.substr(0, nums[i].size()) == nums[i]) {
+                    string complement = target.substr(nums[i].size());
+                    
+                    if (complement == nums[i]) {
+                        count += freq[complement] - 1;
+                    } else {
+                        count += freq[complement];
+                    }
                 }
             }
         }
-        
         return count;
     }
 };
