d602bdc27d160de6587f7ad7ca60109947778290
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-28 00:54:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (95.89%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,16 +1,34 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int n=haystack.size();
-        int m = needle.size();
-        for(int i=0;i<=n-m;i++){
-            int f=i,s=0;
-            while(s<m){
-                if(haystack[f]!=needle[s]) break;
-                else f++,s++;
-            }
-            if(s==m) return f-s;
+    // Rabin Carp Algorithm
+    int strStr(string txt, string pat) {
+        int n = pat.length();
+        int m = txt.length();
+
+        int p = 7, mod = 101;
+        
+        int hashPat = 0, hashText = 0;
+        int pRight = 1, pLeft = 1;
+
+        for(int i = 0; i < n; i++) {
+            hashPat = (hashPat + ((pat[i] - 'a' + 1) * pRight) % mod) % mod;
+            hashText = (hashText + ((txt[i] - 'a' + 1) * pRight) % mod) % mod;
+            pRight = (pRight * p) % mod;
         }
+
+        for(int i = 0; i <= m - n; i++) {
+            if(hashPat == hashText) {
+                if(txt.substr(i, n) == pat) return i;
+            }
+
+            hashText = (hashText - ((txt[i] - 'a' + 1) * pLeft) % mod + mod) % mod;
+            hashText = (hashText + ((txt[i + n] - 'a' + 1) * pRight) % mod) % mod;
+            
+            hashPat = (hashPat * p) % mod;
+            pLeft = (pLeft * p) % mod;
+            pRight = (pRight * p) % mod;
+        }
+
         return -1;
     }
 };
