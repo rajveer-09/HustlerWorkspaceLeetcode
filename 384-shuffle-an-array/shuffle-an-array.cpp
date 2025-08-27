991a39a24cc03966e362ea209689d283965856bb
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-08-27 12:19:04 +0530

    Time: 9 ms (67.44%) | Memory: 63.5 MB (77.17%) - LeetSync

--- 384-shuffle-an-array/shuffle-an-array.cpp
+++ 384-shuffle-an-array/shuffle-an-array.cpp
@@ -15,7 +15,7 @@ public:
     
     vector<int> shuffle() {
         vector<int> shuffled = nums;
-        for (int i = 1; i < shuffled.size(); i++) {
+        for (int i = shuffled.size() - 1; i > 0; i--) {
             int j = rand() % (i + 1); // pick random index in [0, i]
             swap(shuffled[i], shuffled[j]);
         }
