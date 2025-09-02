8a4b36827935dca3943ad4d73eb3c84537b5f907
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-02 18:56:17 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (77.32%) - LeetSync

--- 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 925-construct-binary-tree-from-preorder-and-postorder-traversal/construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     TreeNode* construct(vector<int>& preorder, vector<int>& postorder, int& i1, int& i2){
-        if(i1 == preorder.size()) return NULL;
-
         TreeNode* root = new TreeNode(preorder[i1++]);
-        cout<<i2<<" "<<root->val<<endl;
-        if(root->val == postorder[i2]){
-            i2++;
-            cout<<i2<<"->"<<root->val<<endl;
-            return root;
+
+        // Build left if subtree not closed yet
+        if (root->val != postorder[i2]){
+            root->left = construct(preorder, postorder, i1, i2);
         }
 
-        root->left = construct(preorder, postorder, i1, i2);
-        if(root->val == postorder[i2]){
-            i2++;
-            cout<<i2<<"->"<<root->val<<endl;
-            return root;
+        // Build right if subtree not closed yet
+        if (root->val != postorder[i2]){
+            root->right = construct(preorder, postorder, i1, i2);
         }
-        root->right = construct(preorder, postorder, i1, i2);
 
+        // Now this node is closed in postorder
         i2++;
-
         return root;
-
     }
+
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
         int i1 = 0, i2 = 0;
         return construct(preorder, postorder, i1, i2);
