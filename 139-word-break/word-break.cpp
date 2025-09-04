4f88190ec38375d86a1ba742ee08ae97613deb97
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-04 10:43:43 +0530

    Time: 13 ms (39.26%) | Memory: 16.9 MB (33.51%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,64 +1,23 @@
-class TrieNode {
-public:
-    TrieNode* children[26];
-    bool isTerminal;
-    
-    TrieNode() {
-        for (int i = 0; i < 26; i++) {
-            children[i] = nullptr;
-        }
-        isTerminal = false;
-    }
-};
-
-class Trie {
-public:
-    TrieNode* root;
-
-    Trie() {
-        root = new TrieNode();
-    }
-
-    void insert(string word) {
-        TrieNode* node = root;
-        for (char ch : word) {
-            int index = ch - 'a';
-            if (!node->children[index]) {
-                node->children[index] = new TrieNode();
-            }
-            node = node->children[index];
-        }
-        node->isTerminal = true;
-    }
-};
-
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        Trie t;
-        for (const string& word : wordDict) {
-            t.insert(word);
-        }
-        
-        vector<bool> dp(s.size() + 1, false);
-        dp[0] = true;
+    bool solve(int idx, string& s, unordered_set<string>& st, vector<int>& dp){
+        if(idx == s.size()) return true;
+        if(dp[idx] != -1) return dp[idx];
 
-        for (int i = 0; i < s.size(); i++) {
-            if (dp[i]) {
-                TrieNode* node = t.root;
-                for (int j = i; j < s.size(); j++) {
-                    int index = s[j] - 'a';
-                    if (!node->children[index]) {
-                        break;
-                    }
-                    node = node->children[index];
-                    if (node->isTerminal) {
-                        dp[j + 1] = true;
-                    }
-                }
+        for(int len = 1; len <= s.size() - idx; len++){
+            string part = s.substr(idx, len);
+            if(st.count(part) && solve(idx + len, s, st, dp)){
+                return dp[idx] = true;
             }
         }
         
-        return dp[s.size()];
+        return dp[idx] = false;
+    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+        vector<int> dp(s.size(), -1);
+
+        return solve(0, s, st, dp);
     }
 };
