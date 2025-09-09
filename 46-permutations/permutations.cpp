c9a64acb532d9b3bac3b1cc716e2ab25cbca7e9e
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-10 01:24:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (68.16%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<vector<int>>& ans, int index) {
-        if (index >= nums.size()) {
-            ans.push_back(nums);
-            return;
-        }
-
-        for (int j = index; j < nums.size(); j++) {
-            swap(nums[index], nums[j]);
-            solve(nums, ans, index + 1);
-            swap(nums[index], nums[j]);
-        }
-    }
-
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end()); // ensures lexicographical order
         do {
             ans.push_back(nums);
         } while (next_permutation(nums.begin(), nums.end()));
+
         return ans;
     }
 
 };
+/*
+
+-> The next_permutation(nums.begin(), nums.end()) function in C++ performs two actions:
+-> It modifies the nums container in-place to the next lexicographically greater permutation.
+-> It returns a boolean value:
+1. true if a next lexicographically greater permutation was found and the container was rearranged.
+2. false if the current permutation is the largest possible (e.g., [3, 2, 1]), in which case -> it rearranges the container to the lowest possible order (ascending order, e.g., [1, 2, 3]).
+
+*/
