12b3e3d8eab173bfd71ff9979da0a04293e0cb8e
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-09 20:27:27 +0530

    Time: 5 ms (19.66%) | Memory: 16.4 MB (14.45%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
-    void rec(int n, vector<string>& ans, string &s, int open) {
-        if (s.size() == 2 * n && open == 0) {
-            ans.push_back(s);
+    void solve(int open, int close, string temp, vector<string>& ans, int n){
+        if(open + close == 2 * n){
+            ans.push_back(temp);
             return;
         }
 
-        // if (s.size() > 2 * n) return;
-
-        // place '('
-        if (s.size() < 2 * n) {
-            s += '(';
-            rec(n, ans, s, open + 1);
-            s.pop_back();
+        if(open < n){
+            temp += '(';
+            solve(open + 1, close, temp, ans, n);
+            
+            temp.pop_back();
         }
-
-        // place ')'
-        if (open > 0 && s.size() < 2 * n) {
-            s += ')';
-            rec(n, ans, s, open - 1);
-            s.pop_back();
+        if(close < n  && open > close){
+            temp += ')';
+            solve(open, close + 1, temp, ans, n);
+            temp.pop_back();
         }
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        string s = "";
-        rec(n, ans, s, 0);
+        string temp = "";
+
+        solve(0, 0, temp, ans, n);
+
         return ans;
     }
 };
