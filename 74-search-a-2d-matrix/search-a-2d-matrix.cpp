190aeae078440b101c4431175fc52a3a1e99da13
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-10 13:23:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (76.43%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -6,18 +6,20 @@ public:
 
         int st = 0, ed = m * n - 1;
 
-        while (st <= ed) {
+        while(st <= ed){
             int mid = st + (ed - st) / 2;
+
             int val = matrix[mid / m][mid % m];
 
-            if (val > target) {
-                ed = mid - 1;
-            } 
-            else if (val < target) {
-                st = mid + 1;
-            } 
-            else {
+            if(val == target){
                 return true;
+            }
+
+            if(val > target){
+                ed = mid - 1;
+            }
+            else{
+                st = mid + 1;
             }
         }
 
