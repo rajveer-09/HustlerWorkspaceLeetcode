72c497661cc4d36a1425fb3367d0b84a00e4c2b3
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-13 16:23:19 +0530

    Time: 11 ms (43.73%) | Memory: 19 MB (69.86%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,32 +1,30 @@
 class Solution {
-public:
-    int ans;
-    void solve(TreeNode* root, long long sum, int tsum, unordered_map<long long, int>& mpp){
-        if(!root) return;
-
-        sum += root->val;
-
-        if(sum == tsum) ans++;
+    int total = 0;
+    
+    void findBase(TreeNode* root, int target) {
+        if (!root) return;
+        find(root, target, 0LL);
         
-        if(mpp.count(sum - tsum)) {
-            ans += mpp[sum - tsum];
-        }
-
-        mpp[sum]++;
-
-        solve(root->left, sum, tsum, mpp);
-        solve(root->right, sum, tsum, mpp);
-
-        mpp[sum]--;
+        findBase(root->left, target);
+        findBase(root->right, target);
     }
+    
+    void find(TreeNode* root, int target, long long curr) {
+        if (!root) return;
+        curr += root->val;
 
-    int pathSum(TreeNode* root, int tsum) {
-        ans = 0;
+        if (curr == target) total++;
 
-        unordered_map<long long, int> mpp;
+        find(root->left, target, curr);
+        find(root->right, target, curr);
+    }
+    
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        total = 0;
 
-        solve(root, 0, tsum, mpp);
+        findBase(root, targetSum);
 
-        return ans;
+        return total;
     }
 };
