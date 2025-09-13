34654dad46a3209ca7ebf10e85af6e24696308f0
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-13 18:59:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (24.46%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
+    // we need to reach to every node and swap its righ
+    // and left pointers...be it any way we just have to reach & change
     TreeNode* invertTree(TreeNode* root) {
         if (!root) return nullptr;
+
         queue<TreeNode*> queue;
         queue.push(root);
+
         while (!queue.empty()) {
             TreeNode* node = queue.front();
             queue.pop();
-            
-            if (node->left) queue.push(node->left);
-            if (node->right) queue.push(node->right);
 
             swap(node->left, node->right);
+
+            if (node->left) queue.push(node->left);
+            if (node->right) queue.push(node->right);
         }
+
         return root;
     }
 };
