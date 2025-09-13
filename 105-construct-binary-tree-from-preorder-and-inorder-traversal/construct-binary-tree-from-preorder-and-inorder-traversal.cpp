04bdec9cbb1a62d9ca24ba9799a7258cf6d57c13
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-13 11:58:01 +0530

    Time: 0 ms (100.00%) | Memory: 27 MB (96.75%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,30 +1,26 @@
 class Solution {
-public:
-    unordered_map<int, int> mpp;
-    // pre order aur inorder se root banao
-    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& idx, int st, int ed){
-        if(idx >= preorder.size()) return NULL;
-        if(st > ed) return NULL;
+    int preIdx = 0;
+    int inIdx = 0;
 
-        TreeNode* root = new TreeNode(preorder[idx++]);
-        int idxx = mpp[root->val];
+    TreeNode* dfs(vector<int>& preorder, vector<int>& inorder, int limit) {
+        if (preIdx >= preorder.size()) return nullptr;
 
-        root->left = build(preorder, inorder, idx, st, idxx - 1);
-        
-        root->right = build(preorder, inorder, idx, idxx + 1, ed);
-
-        return root;
-
-    }
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        int n = preorder.size();
-        int idx = 0;
-        for(int i = 0; i < n; i++){
-            mpp[inorder[i]] = i;
+        if (inorder[inIdx] == limit) {
+            inIdx++;
+            return nullptr;
         }
 
-        TreeNode* root = build(preorder, inorder, idx, 0, n - 1);
-
+        TreeNode* root = new TreeNode(preorder[preIdx++]);
+        // left subtree ke saare element node se chhotey honge
+        root->left = dfs(preorder, inorder, root->val);
+        // right subtree ke elem node se bade honge but limit se chhotey honge
+        root->right = dfs(preorder, inorder, limit);
+        
         return root;
+    }
+
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return dfs(preorder, inorder, INT_MAX);
     }
 };
