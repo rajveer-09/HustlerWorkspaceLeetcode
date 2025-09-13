7fa97fffcac989b3c20753e7112e42014b9e7518
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-13 16:24:23 +0530

    Time: 8 ms (56.10%) | Memory: 20.6 MB (46.01%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,10 +1,46 @@
 class Solution {
+public:
+    int ans;
+    void solve(TreeNode* root, long long sum, int tsum, unordered_map<long long, int>& mpp){
+        if(!root) return;
+
+        sum += root->val;
+
+        if(sum == tsum) ans++;
+        
+        if(mpp.count(sum - tsum)) {
+            ans += mpp[sum - tsum];
+        }
+
+        mpp[sum]++;
+
+        solve(root->left, sum, tsum, mpp);
+        solve(root->right, sum, tsum, mpp);
+
+        mpp[sum]--;
+    }
+
+    int pathSum(TreeNode* root, int tsum) {
+        ans = 0;
+
+        unordered_map<long long, int> mpp;
+
+        solve(root, 0, tsum, mpp);
+
+        return ans;
+    }
+};
+
+/*
+<- BRUTE-FORCE ->
+
+class Solution {
     int total = 0;
     
     void findBase(TreeNode* root, int target) {
         if (!root) return;
         find(root, target, 0LL);
-        
+
         findBase(root->left, target);
         findBase(root->right, target);
     }
@@ -28,3 +64,5 @@ public:
         return total;
     }
 };
+
+*/
