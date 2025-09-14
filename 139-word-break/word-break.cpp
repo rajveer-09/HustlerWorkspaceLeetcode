16002541c8dcd62c6d336371b0a1c8b939e5bb32
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 02:17:06 +0530

    Time: 12 ms (41.82%) | Memory: 16.1 MB (47.89%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,23 +1,45 @@
 class Solution {
 public:
-    bool solve(int idx, string& s, unordered_set<string>& st, vector<int>& dp){
-        if(idx == s.size()) return true;
-        if(dp[idx] != -1) return dp[idx];
+    // bool solve(int idx, string& s, unordered_set<string>& st, vector<int>& dp){
+    //     if(idx == s.size()) return true;
+    //     if(dp[idx] != -1) return dp[idx];
 
-        for(int len = 1; len <= s.size() - idx; len++){
-            string part = s.substr(idx, len);
-            if(st.count(part) && solve(idx + len, s, st, dp)){
-                return dp[idx] = true;
+    //     for(int len = 1; len <= s.size() - idx; len++){
+    //         string part = s.substr(idx, len);
+    //         if(st.count(part) && solve(idx + len, s, st, dp)){
+    //             return dp[idx] = true;
+    //         }
+    //     }
+        
+    //     return dp[idx] = false;
+    // }
+
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     unordered_set<string> st(wordDict.begin(), wordDict.end());
+    //     vector<int> dp(s.size(), -1);
+
+    //     return solve(0, s, st, dp);
+    // }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        // unordered_set -> O(1) average time complexity lookups
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                // dp[j] is true, meaning s[0...j-1] can be segmented
+                // s.substr(j, i-j) is a word in the dictionary
+                if (dp[j] && dict.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
         
-        return dp[idx] = false;
-    }
-
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> st(wordDict.begin(), wordDict.end());
-        vector<int> dp(s.size(), -1);
-
-        return solve(0, s, st, dp);
+        return dp[n];
     }
 };
