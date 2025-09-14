a5defb118e958f77c4002d10d023909f8a776a3c
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 01:08:33 +0530

    Time: 8 ms (45.58%) | Memory: 13.2 MB (36.92%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int solve(string& a, string& b, int i, int j, vector<vector<int>>& dp){
-        //base case : 
-        if(i == a.size()) return b.size() - j;
-        if(j == b.size()) return a.size() - i;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int ans = 0;
-
-        if(a[i] == b[j]){
-            ans = solve(a, b, i+1, j+1, dp);
-        }
-
-        else{
-            int ins = 1 + solve(a, b, i, j + 1, dp);
-            int del = 1 + solve(a, b, i+1, j, dp);
-            int rep = 1 + solve(a, b, i+1, j + 1, dp);
-
-            ans = min({ins, del, rep});
-        }
-
-        return dp[i][j] = ans;
-    }
-
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(), vector<int> (word2.size(), -1));
+        int m = word1.length();
+        int n = word2.length();
 
-        return solve(word1, word2, 0, 0, dp);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+        for (int i = 0; i <= m; ++i) {
+            dp[i][0] = i; // Deleting all characters from word1
+        }
+        for (int j = 0; j <= n; ++j) {
+            dp[0][j] = j; // Inserting all characters of word2
+        }
+
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } 
+                else {
+                    dp[i][j] = 1 + min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]});
+                }
+            }
+        }
+
+        return dp[m][n];
     }
 };
