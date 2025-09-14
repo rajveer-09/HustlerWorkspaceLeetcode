b05d7b8653bfc5b4bd85a7696cc1aabeb9c3caaf
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-14 17:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (76.44%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int longestValidParentheses(const string& s) {
-        int n = (int)s.size();
-        vector<int> dp(n, 0);
-        int max_len = 0;
-
-        for (int i = 1; i < n; i++) {
-            if (s[i] == ')') {
-                if (s[i - 1] == '(') {
-                    // Case: "..."
-                    dp[i] = (i >= 2 ? dp[i - 2] : 0) + 2;
-                } 
-                else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '(') {
-                    // Case: "....))"
-                    dp[i] = dp[i - 1] + 2 + ((i - dp[i - 1] - 2) >= 0 ? dp[i - dp[i - 1] - 2] : 0);
-                }
-
-                max_len = max(max_len, dp[i]);
-            }
+    int longestValidParentheses(string s) {
+        int left = 0, right = 0, max_len = 0;
+    
+        // Pass 1: Left to Right
+        for (char c : s) {
+            if (c == '(') left++;
+            else right++;
+            
+            if (left == right)
+                max_len = max(max_len, 2 * right);
+            else if (right > left)
+                left = right = 0;
         }
-
+        
+        left = right = 0;
+        
+        // Pass 2: Right to Left
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '(') left++;
+            else right++;
+            
+            if (left == right)
+                max_len = max(max_len, 2 * left);
+            else if (left > right)
+                left = right = 0;
+        }
+        
         return max_len;
     }
 };
