770ee7d75bc68df637026df736a78aa4e3ff8b04
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 01:50:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (91.19%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -4,26 +4,51 @@ public:
         int m = word1.length();
         int n = word2.length();
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        // vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
 
-        for (int i = 0; i <= m; ++i) {
-            dp[i][0] = i; // Deleting all characters from word1
-        }
-        for (int j = 0; j <= n; ++j) {
-            dp[0][j] = j; // Inserting all characters of word2
+        // for (int i = 0; i <= m; ++i) {
+        //     dp[i][0] = i; // Deleting all characters from word1
+        // }
+        // for (int j = 0; j <= n; ++j) {
+        //     dp[0][j] = j; // Inserting all characters of word2
+        // }
+
+        // for (int i = 1; i <= m; ++i) {
+        //     for (int j = 1; j <= n; ++j) {
+        //         if (word1[i - 1] == word2[j - 1]) {
+        //             dp[i][j] = dp[i - 1][j - 1];
+        //         } 
+        //         else {
+        //             dp[i][j] = 1 + min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]});
+        //         }
+        //     }
+        // }
+        // return dp[m][n];
+
+        vector<int> prev(n + 1, 0), curr(n + 1, 0);
+
+        for(int j = 0; j <= n; j++){
+            // i -> 0 | j -> j
+            prev[j] = j;
         }
 
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } 
-                else {
-                    dp[i][j] = 1 + min({dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]});
+        for(int i = 1; i <= m; i++){
+            curr[0] = i;
+            for(int j = 1; j <= n; j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    curr[j] = prev[j - 1];
+                }
+                else{
+                    // j - 1 is accesible only if j >= 1
+                    curr[j] = 1 + min({prev[j - 1], curr[j - 1], prev[j]});
                 }
             }
+
+            prev = curr;
         }
 
-        return dp[m][n];
+        return prev[n];
+
+        
     }
 };
