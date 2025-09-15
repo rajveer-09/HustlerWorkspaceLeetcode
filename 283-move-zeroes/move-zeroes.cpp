8f784c87c45550a5f04d1843340e4dbe287d2acc
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 19:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (55.32%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int i=0;
-        int j=0;
-        while(j<n){
-            while(nums[i]!=0 && i<n-1) i++;
-            if(i>j) j=i;
-            if(nums[j]!=0){
-                swap(nums[i++],nums[j]);
+        int i = 0, j = 0;
+
+        while(j < nums.size()){
+            // i just have to find non zeroes and put them in order from the start.
+
+            if(nums[j] != 0){
+                swap(nums[i], nums[j]);
+                i++;
             }
+
             j++;
         }
     }
