70a3c2423ba4ebd3805b60dea257dca67e178f2c
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 23:55:37 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (35.05%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-    bool memo(vector<int>& nums, int idx, vector<int>& dp) {
-        if (idx >= nums.size() - 1) return true;
+    // bool memo(vector<int>& nums, int idx, vector<int>& dp) {
+    //     if (idx >= nums.size() - 1) return true;
 
-        if (dp[idx] != -1) return dp[idx];
+    //     if (dp[idx] != -1) return dp[idx];
         
-        // Start from 1 to avoid infinite recursion
-        for (int j = 1; j <= nums[idx]; j++) { 
-            if (memo(nums, idx + j, dp)) {
-                return dp[idx] = true;
-            }
-        }
-        return dp[idx] = false;
-    }
+    //     // Start from 1 to avoid infinite recursion
+    //     for (int j = 1; j <= nums[idx]; j++) { 
+    //         if (memo(nums, idx + j, dp)) {
+    //             return dp[idx] = true;
+    //         }
+    //     }
+        
+    //     return dp[idx] = false;
+    // }
+
+    // bool canJump(vector<int>& nums) {
+    //     vector<int> dp(nums.size(), -1);
+    //     return memo(nums, 0, dp);
+    // }
 
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return memo(nums, 0, dp);
+        int n = nums.size();
+        int farthest = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (i > farthest) {
+                return false;
+            }
+            farthest = max(farthest, i + nums[i]);
+
+            if (farthest >= n - 1) {
+                return true;
+            }
+        }
+
+        return true;
     }
 };
