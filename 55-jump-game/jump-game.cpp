e90286b999b0805c4b7b4c3ab1722bfeb5ddb580
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 21:13:35 +0530

    Time: 623 ms (6.83%) | Memory: 55.9 MB (6.81%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int mx=0;
+    bool memo(vector<int>& nums, int idx, vector<int>& dp) {
+        if (idx >= nums.size() - 1) return true;
 
-        for(int i=0;i<nums.size();i++){
-            if(i>mx) return false;
-            mx= max(mx, i+nums[i]);
+        if (dp[idx] != -1) return dp[idx];
+        
+        // Start from 1 to avoid infinite recursion
+        for (int j = 1; j <= nums[idx]; j++) { 
+            if (memo(nums, idx + j, dp)) {
+                return dp[idx] = true;
+            }
         }
+        return dp[idx] = false;
+    }
 
-        return true;
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+        return memo(nums, 0, dp);
     }
 };
