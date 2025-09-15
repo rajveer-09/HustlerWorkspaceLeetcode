b67eaffb98fcd125c72f3748116aadec516f8d36
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 18:30:42 +0530

    Time: 21 ms (91.70%) | Memory: 17.7 MB (46.73%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,24 +1,72 @@
+class DSU {
+    vector<int> Parent, Size;
+public:
+    DSU(int n) {
+        Parent.resize(n + 1);
+        Size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            Parent[i] = i;
+            Size[i] = 1;
+        }
+    }
+
+    int find(int node) {
+        if (node != Parent[node]) {
+            Parent[node] = find(Parent[node]);
+        }
+        return Parent[node];
+    }
+
+    bool unionBySize(int u, int v) {
+        int pu = find(u);
+        int pv = find(v);
+
+        if (pu == pv) return false;
+        if (Size[pu] >= Size[pv]) {
+            Size[pu] += Size[pv];
+            Parent[pv] = pu;
+        } else {
+            Size[pv] += Size[pu];
+            Parent[pu] = pv;
+        }
+        return true;
+    }
+};
+
 class Solution {
 public:
-    void eat(vector<vector<char>> &grid, int i, int j){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || grid[i][j] != '1') return;
-        grid[i][j] = '0';
-        eat(grid,i+1,j);
-        eat(grid,i,j+1);
-        eat(grid,i-1,j);
-        eat(grid,i,j-1);
-
-    }
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
-        for(int i = 0; i<grid.size(); i++){
-            for(int j = 0; j<grid[0].size(); j++){
-                if(grid[i][j] == '1'){
-                    ans++;
-                    eat(grid,i,j);
+        int ROWS = grid.size();
+        int COLS = grid[0].size();
+        DSU dsu(ROWS * COLS);
+
+        int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int islands = 0;
+
+        auto index = [&](int r, int c) {
+            return r * COLS + c;
+        };
+
+        for (int r = 0; r < ROWS; r++) {
+            for (int c = 0; c < COLS; c++) {
+                if (grid[r][c] == '1') {
+                    islands++;
+
+                    for (auto& d : directions) {
+                        int nr = r + d[0];
+                        int nc = c + d[1];
+
+                        if (nr >= 0 && nc >= 0 && nr < ROWS &&
+                            nc < COLS && grid[nr][nc] == '1') {
+                            if (dsu.unionBySize(index(r, c), index(nr, nc))) {
+                                islands--;
+                            }
+                        }
+                    }
                 }
             }
         }
-        return ans;
+
+        return islands;
     }
 };
