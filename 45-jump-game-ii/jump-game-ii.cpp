895d0632be32aa6b9dd65cde9d01b0c4aea5fd9b
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 20:55:34 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (37.15%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int jmps=0;
-        int lft=0, rht=0;
-
-        for(int i=0;i<nums.size()-1; i++){
-            rht = max(rht, i+nums[i]);
-            if(i==lft){
-                jmps++;
-                lft= rht;
-            }
+        if (nums.size() <= 1) {
+            return 0;
         }
 
-        return jmps;
+        int jumps = 0;
+        int current_end = 0;
+        int farthest = 0;
+
+        // Iterate through all indices except the last one
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            // Update the farthest reach from the current position
+            farthest = max(farthest, i + nums[i]);
+
+            // If we've reached the end of the current jump's range
+            if (i == current_end) {
+                jumps++;
+                current_end = farthest;
+            }
+        }
+        
+        return jumps;
     }
 };
