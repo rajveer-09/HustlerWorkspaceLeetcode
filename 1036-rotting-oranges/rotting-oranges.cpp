0fc16999ded2a40c13412836bd66001d71597530
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-15 19:31:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (60.86%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -3,50 +3,56 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
+
         queue<pair<int, int>> q;
         int freshCount = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(grid[i][j] == 2){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
                     q.push({i, j});
-                }
-                else if(grid[i][j] == 1){
+                } 
+                else if (grid[i][j] == 1) {
                     freshCount++;
                 }
             }
         }
 
-        if(freshCount == 0) return 0;
+        if (freshCount == 0) return 0;
 
-        int cnt = 0;
-        vector<vector<int>> dir = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-        while(!q.empty()){
-            int sz = q.size();
-            bool changed = false;
+        int time = 0;
 
-            for(int i = 0; i < sz; i++){
-                auto [x, y] = q.front(); q.pop();
+        while (!q.empty()) {
+            int size = q.size();
+            bool hasRotten = false;
 
-                for(auto& d : dir){
-                    int xx = x + d[0];
-                    int yy = y + d[1];
+            for (int i = 0; i < size; i++) {
+                auto [x, y] = q.front();
+                q.pop();
 
-                    if(xx >= 0 && xx < n && yy >= 0 && yy < m && grid[xx][yy] == 1){
+                for (auto dir : directions) {
+                    int xx = x + dir.first;
+                    int yy = y + dir.second;
+
+                    if (xx >= 0 && xx < n && yy >= 0 && yy < m && grid[xx][yy] == 1){
                         grid[xx][yy] = 2;
                         q.push({xx, yy});
-                        changed = true;
                         freshCount--;
+                        hasRotten = true;
                     }
                 }
             }
 
-            if(changed) cnt++;
+            if (hasRotten) {
+                time++;
+            }
+            else{
+                break;
+            }
         }
 
-        if(freshCount) return -1;
-
-        return cnt;
+        return freshCount == 0 ? time : -1;
     }
 };
