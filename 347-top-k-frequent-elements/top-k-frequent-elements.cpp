5e1e535d79da4fb6eeb3c55aee36286ef7b541bf
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-16 01:42:54 +0530

    Time: 2 ms (57.42%) | Memory: 20 MB (8.24%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,26 +1,96 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> freqMap;
-        for (int num : nums) {
-            freqMap[num]++;
+        unordered_map<int, int> count;
+        vector<vector<int>> bucket(nums.size() + 1);
+
+        for (int n : nums) {
+            count[n] = 1 + count[n];
+        }
+        for (const auto& entry : count) {
+            bucket[entry.second].push_back(entry.first);
         }
 
-        // Create buckets where index represents frequency
-        vector<vector<int>> buckets(nums.size() + 1);
-        for (auto& entry : freqMap) {
-            buckets[entry.second].push_back(entry.first);
-        }
-
-        // Collect the top k frequent elements
-        vector<int> result;
-        for (int i = buckets.size() - 1; i >= 0 && result.size() < k; --i) {
-            for (int num : buckets[i]) {
-                result.push_back(num);
-                if (result.size() == k) break;
+        vector<int> res;
+        for (int i = bucket.size() - 1; i > 0; --i) {
+            for (int n : bucket[i]) {
+                res.push_back(n);
+                if (res.size() == k) {
+                    return res;
+                }
             }
         }
 
-        return result;
+        return res;
     }
 };
+
+
+/*
+
+<-- BRUTE -->
+Hash Map + Sorting (O(n log n))
+
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int,int> freq;
+    for (int n : nums) freq[n]++;
+
+    vector<pair<int,int>> arr(freq.begin(), freq.end());
+    sort(arr.begin(), arr.end(), [](auto &a, auto &b){
+        return a.second > b.second;
+    });
+
+    vector<int> ans;
+    for (int i = 0; i < k; i++) ans.push_back(arr[i].first);
+    return ans;
+}
+
+<-- BETTER -->
+Heap (O(n log k))
+
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int,int> freq;
+    for (int n : nums) freq[n]++;
+
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> minHeap;
+
+    for (auto &p : freq) {
+        minHeap.push({p.second, p.first});
+        if (minHeap.size() > k) minHeap.pop();
+    }
+
+    vector<int> ans;
+    while (!minHeap.empty()) {
+        ans.push_back(minHeap.top().second);
+        minHeap.pop();
+    }
+    return ans;
+}
+
+
+<-- BEST -->
+Bucket Sort (O(n))
+
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int,int> freq;
+    for (int n : nums) freq[n]++;
+
+    int nSize = nums.size();
+    vector<vector<int>> bucket(nSize + 1);
+
+    for (auto &p : freq) {
+        bucket[p.second].push_back(p.first);
+    }
+
+    vector<int> ans;
+    for (int i = nSize; i >= 0 && ans.size() < k; i--) {
+        for (int num : bucket[i]) {
+            ans.push_back(num);
+            if (ans.size() == k) break;
+        }
+    }
+    return ans;
+}
+
+
+*/
