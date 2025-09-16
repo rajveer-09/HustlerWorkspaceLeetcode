fcbab2eb97369a4761b01f5296db0527a8fa67a3
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-16 21:55:44 +0530

    Time: 0 ms (100.00%) | Memory: 117.8 MB (97.42%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,29 +1,89 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> ans;
+        if (!head || !head->next) return true;
 
-        ListNode* temp = head;
-
-        while(temp != NULL){
-            ans.push_back(temp->val);
-            temp = temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        for(int i = 0; i < ans.size() / 2; i++){
-            if(ans[i] != ans[ans.size() - i - 1]) return false;
+        // Reverse the second half of the list
+        ListNode* prev = nullptr;
+        ListNode* curr = slow;
+        while (curr != nullptr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        // Compare the first half and the reversed second half
+        ListNode* firstHalf = head;
+        ListNode* secondHalf = prev;
+        while (secondHalf != nullptr) {
+            if (firstHalf->val != secondHalf->val) {
+                return false;
+            }
+            firstHalf = firstHalf->next;
+            secondHalf = secondHalf->next;
         }
 
         return true;
     }
 };
+
+/*
+
+<-- RECURSION -->
+
+class Solution {
+    ListNode* cur;
+
+    bool rec(ListNode* node) {
+        if (node != nullptr) {
+            if (!rec(node->next)) {
+                return false;
+            }
+            if (cur->val != node->val) {
+                return false;
+            }
+            cur = cur->next;
+        }
+        return true;
+    }
+
+public:
+    bool isPalindrome(ListNode* head) {
+        cur = head;
+        return rec(head);
+    }
+};
+
+
+<-- STACK BASED ->
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> stack;
+        ListNode* cur = head;
+
+        while (cur != nullptr) {
+            stack.push(cur->val);
+            cur = cur->next;
+        }
+
+        cur = head;
+        while (cur != nullptr && cur->val == stack.top()) {
+            stack.pop();
+            cur = cur->next;
+        }
+
+        return cur == nullptr;
+    }
+};
+
+*/
