1b4824a87417d25e06113d5abc21559974b36cb2
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-16 13:41:05 +0530

    Time: 74 ms (47.06%) | Memory: 148.5 MB (60.60%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -1,39 +1,35 @@
 class MedianFinder {
-    // chhhota....bda || chhota......bda
-    priority_queue<int> pq1; //left
+    /* chhota...bada || chhota...bada */
+    priority_queue<int> pq1; // left
     priority_queue<int, vector<int>, greater<int>> pq2; // right
-public:
-    MedianFinder() {
-        
-    }
-    
-    void addNum(int num) {
-        if(pq1.empty() || pq1.top() > num){
-            pq1.push(num);
-        }
-        else{
-            pq2.push(num);
-        }
 
-        if(pq2.size() > pq1.size()){
+public:
+    MedianFinder() {}
+    // I need to make sure all elem in pq1 are <= pq2
+    void addNum(int num) {
+        // Step 1: Always push into max heap (left)
+        pq1.push(num);
+
+        // Step 2: Balance by moving max from left to right
+        pq2.push(pq1.top());
+        pq1.pop();
+
+        // Step 3: Make sure left (pq1) can have one more than right
+        if (pq1.size() < pq2.size()) {
             pq1.push(pq2.top());
             pq2.pop();
         }
-        if(pq1.size() > pq2.size() + 1){
-            pq2.push(pq1.top());
-            pq1.pop();
-        }
     }
-    
+
     double findMedian() {
-        if(pq1.size() ==pq2.size()){
-            return (pq1.top() *1.0 + pq2.top()*1.0) / 2;
-        }
-        else{
-            return pq1.top();
+        if (pq1.size() == pq2.size()) {
+            return (pq1.top() + pq2.top()) / 2.0;
+        } else {
+            return pq1.top(); // pq1 always has the extra
         }
     }
 };
+
 /**
 \U0001f538 One heap is not enough.
 \U0001f538 Median depends on left & right halves.
