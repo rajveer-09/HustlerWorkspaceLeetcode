6a720085c85290bbac352a4224dd1985e607fc9d
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-16 13:55:32 +0530

    Time: 8 ms (9.61%) | Memory: 79.4 MB (17.49%) - LeetSync

--- 445-add-two-numbers-ii/add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/add-two-numbers-ii.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        stack<int> s1, s2;
+        
+        while (l1) {
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        while (l2) {
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+
+        int carry = 0;
+        ListNode* head = nullptr;
+
+        // process until both stacks & carry empty
+        while (!s1.empty() || !s2.empty() || carry) {
+            int sum = carry;
+            if (!s1.empty()) {
+                sum += s1.top(); s1.pop();
+            }
+            if (!s2.empty()) {
+                sum += s2.top(); s2.pop();
+            }
+            carry = sum / 10;
+            // insert at head
+            ListNode* node = new ListNode(sum % 10);
+            node->next = head;
+            head = node;
+        }
+
+        return head;
+    }
+};
+
+
+/*
+class Solution {
+public:
     ListNode* reverse(ListNode* head){
         ListNode* prev = NULL;
         ListNode* curr = head;
@@ -44,3 +83,5 @@ public:
 
     }
 };
+
+*/
