819a948e400183b64b4e6cb0e667b7be8d6e8921
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-16 21:52:45 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (39.70%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* current = head;
+        
+        while (current != nullptr) {
+            ListNode* nextNode = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        
+        return prev;
+    }
+};
+
+
+/*
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
         if (head == NULL || head->next == NULL) {
             return head;
         }
@@ -11,45 +31,6 @@ public:
         
         head->next = NULL;
         
-        return newHead;
-    }
-};
-
-// class Solution {
-// public:
-//     ListNode* reverseList(ListNode* head) {
-//         ListNode* prev = nullptr;
-//         ListNode* current = head;
-        
-//         while (current != nullptr) {
-//             ListNode* nextNode = current->next;
-//             current->next = prev;
-//             prev = current;
-//             current = nextNode;
-//         }
-        
-//         return prev;
-//     }
-// };
-
-
-/*
-
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-        if (!head) {
-            return nullptr;
-        }
-
-        ListNode* newHead = head;
-        if (head->next) {
-            newHead = reverseList(head->next);
-            head->next->next = head;
-        }
-        
-        head->next = nullptr;
-
         return newHead;
     }
 };
