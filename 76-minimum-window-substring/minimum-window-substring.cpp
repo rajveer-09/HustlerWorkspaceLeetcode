9c197adbeb6bf7564235cf88020d078f8cae46a5
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-17 19:58:19 +0530

    Time: 19 ms (21.48%) | Memory: 11.4 MB (65.74%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,39 +1,41 @@
 class Solution {
 public:
+    bool check(unordered_map<char, int>& mp){
+        for(auto& it : mp){
+            if(it.second > 0) return false;
+        }
+
+        return true;
+    }
     string minWindow(string s, string t) {
-        unordered_map<char,int> k;
-        for(int i = 0; i < t.size(); i++){
-            k[t[i]]++;
-        }
-        
-        int n = s.size();
-        int left = 0, right = 0;
+        unordered_map<char, int> mp;
+
+        for(char it : t) mp[it]++;
+
+        int st = 0;
         int ans = INT_MAX;
-        string result = "";
-        int start ;
-        int cnt = k.size(); // number of unique chars still needed
-        
-        while(right < n){
-            if(k.find(s[right]) != k.end()){
-                k[s[right]]--;
-                if(k[s[right]] == 0) cnt--;
+        int start = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (mp.find(s[i]) != mp.end()) {
+                mp[s[i]]--;
             }
-            
-            while(cnt == 0){
-                if(ans > right - left + 1){
-                    ans = right - left + 1;
-                    start=left;
+
+            while (check(mp)) {
+                if (ans > i - st + 1) {
+                    ans = i - st + 1;
+                    start = st;
                 }
-                if(k.find(s[left]) != k.end()){
-                    k[s[left]]++;
-                    if(k[s[left]] > 0) cnt++;
+                if (mp.find(s[st]) != mp.end()) {
+                    mp[s[st]]++;
                 }
-                left++;
+                
+                st++;
             }
-            right++;
         }
-        if(ans==INT_MAX) return "";
-        result =s.substr(start,ans);
-        return result;
+
+        if (ans == INT_MAX) return "";
+
+        return s.substr(start, ans);
     }
 };
