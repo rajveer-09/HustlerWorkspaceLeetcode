04fd335646a8765e5ef799511afb4b3dc33eb4ad
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-09-18 02:15:52 +0530

    Time: 3 ms (11.14%) | Memory: 13.9 MB (5.74%) - LeetSync

--- 394-decode-string/decode-string.cpp
+++ 394-decode-string/decode-string.cpp
@@ -50,3 +50,38 @@ public:
 
     }
 };
+/*
+
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> countStack;
+        stack<string> strStack;
+        string curr = "";
+        int num = 0;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                num = num * 10 + (c - '0'); // build number
+            } else if (c == '[') {
+                countStack.push(num);
+                strStack.push(curr);
+                num = 0;
+                curr = "";
+            } else if (c == ']') {
+                int times = countStack.top(); countStack.pop();
+                string prev = strStack.top(); strStack.pop();
+                while (times--) prev += curr;
+                curr = prev;
+            } else {
+                curr += c; // append normal char
+            }
+        }
+
+        return curr;
+    }
+};
+
+
+
+*/
