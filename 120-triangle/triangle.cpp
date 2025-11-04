f3c3febee70cf285f0161fd1e72621fc55401458
Rajveer Sharma <rajveersharma1107@gmail.com>
2025-11-04 10:57:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (15.12%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
+    // define the rec fun
+    // min sum
+    int solve(vector<vector<int>>& triangle, int row, int col, vector<vector<int>>& dp){
+        int n = triangle.size();
+        if(row == n){
+            return 0;
+        }
+
+        if(dp[row][col] != INT_MAX) return dp[row][col];
+
+        int ans = INT_MAX;
+
+        int down = solve(triangle, row + 1, col, dp);
+        int diag = solve(triangle , row + 1, col + 1, dp);
+
+        return dp[row][col] = triangle[row][col] + min(down, diag);
+        
+    }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        int m = triangle[n - 1].size();
+        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
 
-        // Base case: fill the last row as is
-        for (int j = 0; j < triangle[n - 1].size(); ++j) {
-            dp[n - 1][j] = triangle[n - 1][j];
-        }
-
-        // Build from bottom up
-        for (int i = n - 2; i >= 0; --i) {
-            for (int j = 0; j < triangle[i].size(); ++j) {
-                dp[i][j] = triangle[i][j] + min(dp[i + 1][j], dp[i + 1][j + 1]);
-            }
-        }
-
-        // Top element is the answer
-        return dp[0][0];
+        return solve(triangle, 0, 0, dp);
     }
 };
-
-
